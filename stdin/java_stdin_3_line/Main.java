246c8b164c8215d5d2365cda6563ab00ba0ed42b
kos-y <kosyos.address@gmail.com>
2025-03-29 20:14:44 +0900

    Paiza 問題集 3行のデータの入力 Java

--- stdin/java_stdin_3_line/Main.java
+++ stdin/java_stdin_3_line/Main.java
@@ -1,18 +1,15 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
-        String[] lines = new String[3];
-        
-        for (int i = 0; i < lines.length; i++) {
-            lines[i] = sc.nextLine();
-        }
+        final int lineCount = 3;
+        Scanner scanner = new Scanner(System.in);
 
-        for (String line : lines) {
-            System.out.println(line);
-        }
+        IntStream.range(0, lineCount)
+                 .mapToObj(i -> scanner.nextLine())
+                 .forEach(System.out::println); 
     }
 }
