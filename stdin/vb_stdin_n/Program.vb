e020b8431f3441a6c732721c9d6722daba9fb85c
kos-y <kosyos.address@gmail.com>
2025-03-30 18:20:25 +0900

    Paiza 問題集 N個のデータの入力 Visual Basic

---
+++ stdin/vb_stdin_n/Program.vb
@@ -0,0 +1,15 @@
+Imports System
+Imports System.Linq
+
+Public Class Program
+    Shared Function Main() As Integer
+        Console.ReadLine()
+
+        Dim words() As String = Console.ReadLine().Split()
+        For Each word As String In words
+            Console.WriteLine(word)
+        Next
+
+        Return 0
+    End Function
+End Class
