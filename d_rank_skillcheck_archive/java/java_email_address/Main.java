848134a60e9dfcc1de00bb7f5d8049b3b5a6e9eb
kos-y <kosyos.address@gmail.com>
2025-04-06 19:08:14 +0900

    Paiza 問題集 Dランク・スキルチェック過去問題セット Java

---
+++ d_rank_skillcheck_archive/java/java_email_address/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+        String t = sc.nextLine();
+        System.out.printf("%s@%s", s, t);
+    }
+}
