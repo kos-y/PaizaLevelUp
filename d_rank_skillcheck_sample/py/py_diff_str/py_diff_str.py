08f92d5299c9c8d3b489dd438bb3f697389b2027
kos-y <kosyos.address@gmail.com>
2025-04-06 16:02:13 +0900

    Paiza 問題集 Dランク・スキルチェック見本問題セット Python 3

---
+++ d_rank_skillcheck_sample/py/py_diff_str/py_diff_str.py
@@ -0,0 +1,4 @@
+# coding: utf-8
+a = input()
+b = input()
+print("OK" if a == b else "NG")
---
+++ d_rank_skillcheck_sample/py/py_min_num/py_min_num.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+min_value = 101
+for _ in range(5):
+    v = int(input())
+    if v < min_value:
+        min_value = v
+print(min_value)
