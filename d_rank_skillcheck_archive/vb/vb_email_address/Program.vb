6c3c88871f0bc34b3afbe33c88523fe31d746c57
kos-y <kosyos.address@gmail.com>
2025-04-06 21:23:34 +0900

    Paiza 問題集 Dランク・スキルチェック過去問題セット Visual Basic

---
+++ d_rank_skillcheck_archive/vb/vb_email_address/Program.vb
@@ -0,0 +1,10 @@
+Imports System
+
+Public Class Program
+    Shared Sub Main()
+        Dim s As String = Console.ReadLine()
+        Dim t As String = Console.ReadLine()
+
+        Console.WriteLine(s & "@" & t)
+    End Sub
+End Class
---
+++ d_rank_skillcheck_archive/vb/vb_square/Program.vb
@@ -0,0 +1,9 @@
+Imports System
+
+Public Class Program
+    Shared Sub Main()
+        Dim n As Integer = Integer.Parse(Console.ReadLine())
+
+        Console.WriteLine(New String("*"c, n))
+    End Sub
+End Class
