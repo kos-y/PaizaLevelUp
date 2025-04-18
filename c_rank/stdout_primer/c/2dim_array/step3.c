2012db1f531ba3a1080a9a658499b6bc9471881d
kos-y <kosyos.address@gmail.com>
2025-04-19 08:31:09 +0900

    標準出力メニュー 九九表の出力 C編（paizaランク D 相当））

--- c_rank/stdout_primer/c/2dim_array/step3.c
+++ c_rank/stdout_primer/c/2dim_array/step3.c
@@ -1,4 +1,7 @@
+// Paiza 問題集 標準出力メニュー 九九表の出力 C編（paizaランク D 相当））
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__2dim_array_step3
 #include <stdio.h>
+
 #define N 9
 
 int main()
@@ -9,10 +12,9 @@ int main()
             if (j < N) {
                 printf(" ");
             }
-            else {
-                printf("\n");
-            }
+
         }
+        printf("\n");
     }
 
     return 0;
