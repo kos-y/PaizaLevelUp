501398538d540ad82b8bae5159e4a36ee011d4a1
kos-y <kosyos.address@gmail.com>
2025-04-18 23:45:21 +0900

    Paiza 問題集 Dランクレベルアップメニュー 入力された 引き算・掛け算 C編

--- d_rank/d_rank_level_up_problems/c/accompanied_by_stdin/2.c
+++ d_rank/d_rank_level_up_problems/c/accompanied_by_stdin/2.c
@@ -1,4 +1,5 @@
 // Paiza 問題集 Dランクレベルアップメニュー 入力された 引き算・掛け算 C編
+// https://paiza.jp/works/mondai/d_rank_level_up_problems/d_rank_level_up_problems__accompanied_by_stdin_2
 #include <stdio.h>
 
 int main()
