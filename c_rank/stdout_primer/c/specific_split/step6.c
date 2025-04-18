faaea67259f4c29a36c335be96a2110d3e44e363
kos-y <kosyos.address@gmail.com>
2025-04-19 00:44:11 +0900

    Paiza 問題集 標準出力メニュー 大きな数値を 3 けたごとにカンマ区切りで出力 C編（paizaランク C 相当）

--- c_rank/stdout_primer/c/specific_split/step6.c
+++ c_rank/stdout_primer/c/specific_split/step6.c
@@ -1,17 +1,18 @@
+// Paiza 問題集 標準出力メニュー 大きな数値を 3 けたごとにカンマ区切りで出力 C編（paizaランク C 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__specific_split_step6
 #include <stdio.h>
 #include <string.h>
 
 int main()
 {
-    char n[1024];
-    scanf("%s", n);
+    char base[1024];
+    scanf("%s", base);
     
     char s[2048] = "";
     int j = 0;
-    for (int i = strlen(n) - 1, k = 0; i >= 0; i--, j++) {
-        s[j] = n[i];
-        k++;
-        if (k == 3 && i > 0) {
+    for (int i = strlen(base) - 1, k = 0; i >= 0; i--, j++) {
+        s[j] = base[i];
+        if (++k == 3 && i > 0) {
             s[++j] = ',';
             k = 0;
         }
@@ -21,7 +22,7 @@ int main()
     for (int i = strlen(s) - 1; i >= 0; i--) {
         printf("%c", s[i]);
     }
-    printf("\n");
 
+    printf("\n");
     return 0;
 }
