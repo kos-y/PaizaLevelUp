05db88bc24cac277c161490f0481904f0ac1a51b
kos-y <kosyos.address@gmail.com>
2025-04-18 22:25:51 +0900

    Paiza 問題集 Dランクレベルアップメニュー 累乗 C編

--- d_rank/d_rank_level_up_problems/c/arithmetic_substitution_op/3.c
+++ d_rank/d_rank_level_up_problems/c/arithmetic_substitution_op/3.c
@@ -1,3 +1,4 @@
+// Paiza 問題集 Dランクレベルアップメニュー 累乗 C編
 #include <stdio.h>
 #include <math.h>
 
@@ -5,6 +6,8 @@ int main()
 {
     int a = 202, b = 134, c = 107;
 
-    printf("%.0lf\n", pow((a + b) * c, 2));
+    int n = (int)pow((a + b) * c, 2);
+
+    printf("%d\n", n);
     return 0;
 }
