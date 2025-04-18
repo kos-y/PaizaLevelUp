2d0266ca12a8611993c04eb5c0f168b41654a47b
kos-y <kosyos.address@gmail.com>
2025-04-18 23:40:13 +0900

    Paiza 問題集 Dランクレベルアップメニュー 代入演算 1 C編

--- d_rank/d_rank_level_up_problems/c/arithmetic_substitution_op/boss.c
+++ d_rank/d_rank_level_up_problems/c/arithmetic_substitution_op/boss.c
@@ -1,4 +1,5 @@
 // Paiza 問題集 Dランクレベルアップメニュー 代入演算 1 C編
+// https://paiza.jp/works/mondai/d_rank_level_up_problems/d_rank_level_up_problems__arithmetic_substitution_op_boss
 #include <stdio.h>
 
 int main()
