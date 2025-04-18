6b017ccb1c71c45afaf1d365942bb3022ee063c1
kos-y <kosyos.address@gmail.com>
2025-04-19 08:47:51 +0900

    Paiza 問題集 標準出力 1 つの実数を出力 4 C編（paizaランク C 相当）

--- c_rank/stdout_primer/c/format_real_number/step4.c
+++ c_rank/stdout_primer/c/format_real_number/step4.c
@@ -1,13 +1,14 @@
+// Paiza 問題集 標準出力 1 つの実数を出力 4 C編（paizaランク C 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__format_real_number_step4
 #include <stdio.h>
 
 int main()
 {
     double n;
     int m;
+    char f[6];
 
     scanf("%lf %d", &n, &m);
-
-    char f[6];
     sprintf(f, "%%.%dlf", m);
     printf(f, n);
     return 0;
