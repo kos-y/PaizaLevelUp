a9d6fd48331a769ff01615602e362a6fd16b2baa
kos-y <kosyos.address@gmail.com>
2025-04-19 00:30:54 +0900

    Paiza 問題集 標準出力メニュー 【n 行の出力】1,000 行以内の出力 C編（paizaランク D 相当）

--- c_rank/stdout_primer/c/n_line/boss.c
+++ c_rank/stdout_primer/c/n_line/boss.c
@@ -1,10 +1,12 @@
+// Paiza 問題集 標準出力メニュー 【n 行の出力】1,000 行以内の出力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__n_line_boss
 #include <stdio.h>
 
 int main()
 {
     int n;
-    scanf("%d", &n);
 
+    scanf("%d", &n);
     for (int i = 1; i <= n; i++) {
         printf("%d\n", i);
     }
