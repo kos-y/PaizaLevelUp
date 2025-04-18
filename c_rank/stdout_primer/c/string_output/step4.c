ed86966f2e8fd123fc90519d2bca67bcc970c2fa
kos-y <kosyos.address@gmail.com>
2025-04-19 00:19:28 +0900

    Paiza 問題集 入力された 10 個の文字列を出力 C編（paizaランク D 相当）

--- c_rank/stdout_primer/c/string_output/step4.c
+++ c_rank/stdout_primer/c/string_output/step4.c
@@ -1,23 +1,25 @@
+// Paiza 問題集 入力された 10 個の文字列を出力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__string_output_step4
 #include <stdio.h>
+
 #define COUNT 10
 #define LEN 10
 
 int main()
 {
-    char s[COUNT][LEN + 1], f[5];
+    char s[COUNT][LEN + 1];
+    char format[5];
 
-    sprintf(f, "%%%ds", LEN);
+    sprintf(format, "%%%ds", LEN);
     for (int i = 0; i < COUNT; i++) {
-        scanf(f, s[i]);
-    }
+        scanf(format, s[i]);
 
-    for (int i = 0; i < COUNT; i++) {
         printf("%s", s[i]);
         if (i < COUNT - 1) {
             printf(" ");
         }
     }
-    printf("\n");
 
+    printf("\n");
     return 0;
 }
