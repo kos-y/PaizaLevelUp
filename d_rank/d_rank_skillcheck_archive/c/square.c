ee718894db4c46d2449934c7c87a38be0e5f8758
kos-y <kosyos.address@gmail.com>
2025-04-18 23:02:20 +0900

    Paiza 問題集 Dランク・スキルチェック過去問題セット N倍の文字列 C編

--- d_rank/d_rank_skillcheck_archive/c/square.c
+++ d_rank/d_rank_skillcheck_archive/c/square.c
@@ -1,13 +1,16 @@
+// Paiza 問題集 Dランク・スキルチェック過去問題セット N倍の文字列 C編
+// https://paiza.jp/works/mondai/d_rank_skillcheck_archive/square
 #include <stdio.h>
 
 int main()
 {
     int n;
-
     scanf("%d", &n);
+
     for (int i = 0; i < n; i++) {
         printf("*");
     }
+
     printf("\n");
     return 0;
 }
