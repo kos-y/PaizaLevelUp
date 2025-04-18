1083e56c79a6b265c065a85219f4248e0b2601d8
kos-y <kosyos.address@gmail.com>
2025-04-19 00:00:00 +0900

    Paiza 問題集 標準出力メニュー 2 行の出力 C編（paizaランク D 相当）

--- c_rank/stdout_primer/c/newline_digit/step2.c
+++ c_rank/stdout_primer/c/newline_digit/step2.c
@@ -1,10 +1,14 @@
+// Paiza 問題集 標準出力メニュー 2 行の出力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__newline_digit_step2
 #include <stdio.h>
+
+#define NUM_ELE(x) (sizeof(x) / sizeof(x[0]))
 
 int main()
 {
     int vs[] = {8, 13};
 
-    for (int i = 0; i < sizeof(vs) / sizeof(vs[0]); i++) {
+    for (int i = 0; i < NUM_ELE(vs); i++) {
         printf("%d\n", vs[i]);
     }
     return 0;
