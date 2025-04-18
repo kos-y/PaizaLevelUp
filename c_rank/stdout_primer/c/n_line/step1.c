d7452f46a287a96beddd8b377d8b37c989536577
kos-y <kosyos.address@gmail.com>
2025-04-19 00:26:46 +0900

    Paiza 問題集 入力された 1 行または 2 行の出力 C編（paizaランク D 相当）

--- c_rank/stdout_primer/c/n_line/step1.c
+++ c_rank/stdout_primer/c/n_line/step1.c
@@ -1,10 +1,12 @@
+// Paiza 問題集 入力された 1 行または 2 行の出力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__n_line_step1
 #include <stdio.h>
 
 int main()
 {
     int n;
+    
     scanf("%d", &n);
-
     for (int i = 1; i <= n; i++) {
         printf("%d\n", i);
     }
