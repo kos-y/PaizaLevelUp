ec3774e6d5cb3cca61b4b97d341e428c8b3b70ce
kos-y <kosyos.address@gmail.com>
2025-04-19 00:17:15 +0900

    Paiza 問題集 入力された 2 つの文字列を出力 C編（paizaランク D 相当）

--- c_rank/stdout_primer/c/string_output/step3.c
+++ c_rank/stdout_primer/c/string_output/step3.c
@@ -1,12 +1,19 @@
+// Paiza 問題集 入力された 2 つの文字列を出力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__string_output_step3
 #include <stdio.h>
+
 #define LEN 10
 
 int main()
 {
     char s[LEN + 1], t[LEN + 1];
+    char format[5];
 
-    scanf("%s ", s);
-    scanf("%s ", t);
-    printf("%s\n%s\n", s, t);
+    sprintf(format, "%%%ds", LEN);
+    scanf(format, s);
+    scanf(format, t);
+
+    printf("%s\n", s);
+    printf("%s\n", t);
     return 0;
 }
