f814edf94f230808aeaea4a4c53dcb127c608fec
kos-y <kosyos.address@gmail.com>
2025-04-18 23:04:02 +0900

    Paiza 問題集 自己紹介問題セット 自己紹介 C編（paizaランク D 相当）

--- first_step/self_introduction/c.c
+++ first_step/self_introduction/c.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 自己紹介問題セット 自己紹介 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/self_introduction/self_introduction__1
 #include <stdio.h>
 
 int main()
@@ -5,6 +7,5 @@ int main()
     printf("name: kos-y\n");
     printf("language: C\n");
     printf("hitokoto: kos-y C");
-
     return 0;
 }
