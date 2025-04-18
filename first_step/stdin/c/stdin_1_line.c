136790b58b9fe0e578971fcf96ec54a5dc936fd4
kos-y <kosyos.address@gmail.com>
2025-04-18 23:09:08 +0900

    Paiza 問題集 標準入力サンプル問題セット 1行のデータの入力 C編（paizaランク D 相当）

--- first_step/stdin/c/stdin_1_line.c
+++ first_step/stdin/c/stdin_1_line.c
@@ -1,15 +1,17 @@
+// Paiza 問題集 標準入力サンプル問題セット 1行のデータの入力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin/stdin_1_line
 #include <stdio.h>
+#include <string.h>
+
+#define LEN 100
 
 int main()
 {
-    char line[101];
+    char s[LEN + 2];
 
-    // 入力
-    fgets(line, sizeof(line), stdin);
+    fgets(s, sizeof(s), stdin);
+    s[strcspn(s, "\n")] = '\0';
 
-    // 出力
-    // fgetsは改行コードも含まれている
-    printf("%s", line);
-
+    printf("%s\n", s);
     return 0;
 }
