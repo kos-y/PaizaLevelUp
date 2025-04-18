667c142663f025a7f6e9cfc65f5e1a26d025fc9d
kos-y <kosyos.address@gmail.com>
2025-04-18 22:43:22 +0900

    Paiza 問題集 Dランクレベルアップメニュー 入力された 引き算・掛け算 C編

--- d_rank/d_rank_level_up_problems/c/accompanied_by_stdin/2.c
+++ d_rank/d_rank_level_up_problems/c/accompanied_by_stdin/2.c
@@ -1,3 +1,4 @@
+// Paiza 問題集 Dランクレベルアップメニュー 入力された 引き算・掛け算 C編
 #include <stdio.h>
 
 int main()
@@ -5,6 +6,10 @@ int main()
     int a, b;
 
     scanf("%d %d", &a, &b);
-    printf("%d %d\n", a - b, a * b);
+
+    int d = a - b;
+    int p = a * b;
+    
+    printf("%d %d\n", d, p);
     return 0;
 }
