075fcff557f36699bf3476d88d276db91dea6338
kos-y <kosyos.address@gmail.com>
2025-04-19 00:38:09 +0900

    Paiza 問題集 標準出力メニュー カンマ区切りで 10 個出力 2 C編（paizaランク D 相当）

--- c_rank/stdout_primer/c/specific_split/step4.c
+++ c_rank/stdout_primer/c/specific_split/step4.c
@@ -1,4 +1,7 @@
+// Paiza 問題集 標準出力メニュー カンマ区切りで 10 個出力 2 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__specific_split_step4
 #include <stdio.h>
+
 #define N 10
 
 int main()
@@ -7,15 +10,12 @@ int main()
 
     for (int i = 0; i < N; i++) {
         scanf("%d", &n[i]);
-    }
-
-    for (int i = 0; i < N; i++) {
         printf("%d", n[i]);
         if (i < N - 1) {
             printf(",");
         }
     }
-    printf("\n");
 
+    printf("\n");
     return 0;
 }
