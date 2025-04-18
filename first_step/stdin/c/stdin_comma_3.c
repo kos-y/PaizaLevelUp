6df02e1f8a0e93d8b0c12cb201979dd4ef64a2d8
kos-y <kosyos.address@gmail.com>
2025-04-18 23:25:52 +0900

    Paiza 問題集 標準入力サンプル問題セット カンマ区切りの3つのデータの入力 C編（paizaランク D 相当）

--- first_step/stdin/c/stdin_comma_3.c
+++ first_step/stdin/c/stdin_comma_3.c
@@ -1,18 +1,22 @@
+// Paiza 問題集 標準入力サンプル問題セット カンマ区切りの3つのデータの入力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin/stdin_comma_3
 #include <stdio.h>
 #include <string.h>
 
+#define COUNT 3
+#define LEN 100
+
 int main()
 {
-    char line[303];
-    
-    // 入力
-    fgets(line, sizeof(line), stdin);
+    char s[(LEN * COUNT) + COUNT + 1];
 
-    // カンマ区切りで文字列を取得して出力
-    char* p_token = strtok(line, ",");
-    while (p_token != NULL) {
-        printf("%s\n", p_token);
-        p_token = strtok(NULL, ",");
+    fgets(s, sizeof(s), stdin);
+    s[strcspn(s, "\n")] = '\0';
+
+    char* t = strtok(s, ",");
+    while (t != NULL) {
+        printf("%s\n", t);
+        t = strtok(NULL, ",");
     }
 
     return 0;
