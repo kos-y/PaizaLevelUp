afb0dcfc42c669a28e8426329e5583fab99b9c3b
kos-y <kosyos.address@gmail.com>
2025-04-18 22:30:20 +0900

    Paiza 問題集 Dランクレベルアップメニュー 1 行の入力 C編

--- d_rank/d_rank_level_up_problems/c/stdin/1.c
+++ d_rank/d_rank_level_up_problems/c/stdin/1.c
@@ -1,10 +1,16 @@
+// Paiza 問題集 Dランクレベルアップメニュー 1 行の入力 C編
 #include <stdio.h>
+
+#define LEN 10
 
 int main()
 {
-    char s[11];
+    char s[LEN + 1];
+    char format[5];
 
-    fgets(s, sizeof(s), stdin);
+    sprintf(format, "%%%ds", LEN);
+    scanf(format, s);
+
     printf("%s\n", s);
     return 0;
 }
