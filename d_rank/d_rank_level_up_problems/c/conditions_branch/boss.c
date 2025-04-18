ab77c3b76954c0a92f8e6ba0d1af9a02d8f80569
kos-y <kosyos.address@gmail.com>
2025-04-18 22:54:13 +0900

    Paiza 問題集 Dランクレベルアップメニュー 占い C編

--- d_rank/d_rank_level_up_problems/c/conditions_branch/boss.c
+++ d_rank/d_rank_level_up_problems/c/conditions_branch/boss.c
@@ -1,10 +1,19 @@
+// Paiza 問題集 Dランクレベルアップメニュー 占い C編
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
     int n;
-
     scanf("%d", &n);
-    printf("%s\n", n == 7 ? "Yes" : "No");
+
+    char a[4];
+    if (n == 7) {
+        strcpy(a, "Yes");
+    } else {
+        strcpy(a, "No");
+    }
+
+    printf("%s\n", a);
     return 0;
 }
