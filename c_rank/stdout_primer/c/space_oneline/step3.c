0e5576b02306d1931a59b852184bdf912bebeaae
kos-y <kosyos.address@gmail.com>
2025-04-19 00:10:14 +0900

    Paiza 問題集 標準出力メニュー 10 個の数値を出力 (末尾に半角スペース有) C編（paizaランク D 相当）

--- c_rank/stdout_primer/c/space_oneline/step3.c
+++ c_rank/stdout_primer/c/space_oneline/step3.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 標準出力メニュー 10 個の数値を出力 (末尾に半角スペース有) C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__space_oneline_step3
 #include <stdio.h>
 
 int main()
@@ -5,7 +7,7 @@ int main()
     for (int i = 1; i <= 10; i++) {
         printf("%d ", i);
     }
+    
     printf("\n");
-
     return 0;
 }
