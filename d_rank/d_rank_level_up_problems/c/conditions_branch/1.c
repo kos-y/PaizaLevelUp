ef15a586efe1a674c2cadcd84f86af64a8f4d18f
kos-y <kosyos.address@gmail.com>
2025-04-18 22:48:36 +0900

    Paiza 問題集 Dランクレベルアップメニュー 単純な条件分岐 C編

--- d_rank/d_rank_level_up_problems/c/conditions_branch/1.c
+++ d_rank/d_rank_level_up_problems/c/conditions_branch/1.c
@@ -1,11 +1,19 @@
+// Paiza 問題集 Dランクレベルアップメニュー 単純な条件分岐 C編
 #include <stdio.h>
 #include <string.h>
 
 int main()
 {
     char s[20];
-
     scanf("%s", s);
-    printf("%s\n", strcmp(s, "paiza") == 0 ? "YES" : "NO");
+
+    char a[4];
+    if (strcmp(s, "paiza") == 0) {
+        strcpy(a, "YES");
+    } else {
+        strcpy(a, "NO");
+    }
+
+    printf("%s\n", a);
     return 0;
 }
