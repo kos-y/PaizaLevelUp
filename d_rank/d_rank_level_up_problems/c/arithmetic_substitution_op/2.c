33488148567145c12ecd39306394c6dacd38b413
kos-y <kosyos.address@gmail.com>
2025-04-18 23:39:03 +0900

    Paiza 問題集 Dランクレベルアップメニュー 割り算 C編

--- d_rank/d_rank_level_up_problems/c/arithmetic_substitution_op/2.c
+++ d_rank/d_rank_level_up_problems/c/arithmetic_substitution_op/2.c
@@ -1,4 +1,5 @@
 // Paiza 問題集 Dランクレベルアップメニュー 割り算 C編
+// https://paiza.jp/works/mondai/d_rank_level_up_problems/d_rank_level_up_problems__arithmetic_substitution_op_2
 #include <stdio.h>
 
 int main()
