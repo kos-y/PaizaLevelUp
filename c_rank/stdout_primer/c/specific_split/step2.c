fa9bf5ffb850a8d2df2b7c71c71c872effdb4b45
kos-y <kosyos.address@gmail.com>
2025-04-19 00:35:02 +0900

    Paiza 問題集 標準出力メニュー バーティカルライン区切りで 3 つの文字列を出力 C編（paizaランク D 相当）

--- c_rank/stdout_primer/c/specific_split/step2.c
+++ c_rank/stdout_primer/c/specific_split/step2.c
@@ -1,4 +1,7 @@
+// Paiza 問題集 標準出力メニュー バーティカルライン区切りで 3 つの文字列を出力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__specific_split_step2
 #include <stdio.h>
+
 #define N 3
 #define LEN 10
 #define F_LEN 4
@@ -11,15 +14,13 @@ int main()
     sprintf(f, "%%%ds", LEN);
     for (int i = 0; i < N; i++) {
         scanf(f, s[i]);
-    }
 
-    for (int i = 0; i < N; i++) {
         printf("%s", s[i]);
         if (i < N - 1) {
             printf("|");
         }
     }
-    printf("\n");
 
+    printf("\n");
     return 0;
 }
