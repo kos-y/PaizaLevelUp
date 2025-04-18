1604d7e89a8e42d55f6f051dcb559811383f7ef9
kos-y <kosyos.address@gmail.com>
2025-04-18 23:54:23 +0900

    Paiza 問題集 Dランク・スキルチェック見本セット 足し算 C編

--- d_rank/d_rank_skillcheck_sample/c/addition.c
+++ d_rank/d_rank_skillcheck_sample/c/addition.c
@@ -1,11 +1,13 @@
 // Paiza 問題集 Dランク・スキルチェック見本セット 足し算 C編
+// https://paiza.jp/works/mondai/d_rank_skillcheck_sample/addition
 #include <stdio.h>
 
 int main()
 {
     int a, b;
-
     scanf("%d %d", &a, &b);
-    printf("%d\n", a + b);
+
+    int x = a + b;
+    printf("%d\n", x);
     return 0;
 }
