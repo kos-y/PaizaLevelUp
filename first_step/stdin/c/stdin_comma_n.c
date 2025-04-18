a48c96166cad43e1ab8c0693f30730ade127fffa
kos-y <kosyos.address@gmail.com>
2025-04-18 23:32:43 +0900

    Paiza 問題集 標準入力サンプル問題セット カンマ区切りのN個のデータの入力 C編（paizaランク C 相当）

--- first_step/stdin/c/stdin_comma_n.c
+++ first_step/stdin/c/stdin_comma_n.c
@@ -1,22 +1,25 @@
+// Paiza 問題集 標準入力サンプル問題セット カンマ区切りのN個のデータの入力 C編（paizaランク C 相当）
+// https://paiza.jp/works/mondai/stdin/stdin_comma_n
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+#define LEN 100
 
 int main()
 {
-    // 文字列数の入力
-    int word_count;
-    scanf("%d ", &word_count);
+    int n;
+    scanf("%d ", &n);
 
-    // 文字列の入力
-    char line[1011];
-    fgets(line, sizeof(line), stdin);
+    char* s = (char*)calloc((LEN * n) + n + 1, sizeof(char));
+    scanf("%s", s);
 
-    // カンマ区切りで文字列を取得して出力
-    char* p_token = strtok(line, ",");
-    while (p_token != NULL) {
-        printf("%s\n", p_token);
-        p_token = strtok(NULL, ",");
+    char* t = strtok(s, ",");
+    while (t != NULL) {
+        printf("%s\n", t);
+        t = strtok(NULL, ",");
     }
 
+    free(s);
     return 0;
 }
