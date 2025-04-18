3589928e47fe6c32edf39e0debe578117b82365b
kos-y <kosyos.address@gmail.com>
2025-04-18 22:46:24 +0900

    Paiza 問題集 Dランクレベルアップメニュー 乗客人数 C編

--- d_rank/d_rank_level_up_problems/c/accompanied_by_stdin/boss.c
+++ d_rank/d_rank_level_up_problems/c/accompanied_by_stdin/boss.c
@@ -1,10 +1,12 @@
+// Paiza 問題集 Dランクレベルアップメニュー 乗客人数 C編
 #include <stdio.h>
 
 int main()
 {
     int a, b, c;
-
     scanf("%d %d %d", &a, &b, &c);
-    printf("%d\n", a - b + c);
+
+    int x = a - b + c;
+    printf("%d\n", x);
     return 0;
 }
