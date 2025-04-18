3d983d1261dc0b90aeeff571258f4d4c36706245
kos-y <kosyos.address@gmail.com>
2025-04-18 22:41:37 +0900

    Paiza 問題集 Dランクレベルアップメニュー 入力された 2 つの文字列を出力 C編

--- d_rank/d_rank_level_up_problems/c/accompanied_by_stdin/1.c
+++ d_rank/d_rank_level_up_problems/c/accompanied_by_stdin/1.c
@@ -1,11 +1,17 @@
+// Paiza 問題集 Dランクレベルアップメニュー 入力された 2 つの文字列を出力 C編
 #include <stdio.h>
+
+#define LEN 10
 
 int main()
 {
-    char s[11], t[11];
+    char s[LEN + 1], t[LEN + 1];
+    char format[5];
 
-    scanf("%s", s);
-    scanf("%s", t);
+    sprintf(format, "%%%ds", LEN);
+    scanf(format, s);
+    scanf(format, t);
+    
     printf("%s\n", s);
     printf("%s\n", t);
     return 0;
