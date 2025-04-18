26f2ec9f355363a8abf0f88e796aaacb99d16ae5
kos-y <kosyos.address@gmail.com>
2025-04-18 23:44:30 +0900

    Paiza 問題集 Dランクレベルアップメニュー 入力された 2 つの文字列を出力 C編

--- d_rank/d_rank_level_up_problems/c/accompanied_by_stdin/1.c
+++ d_rank/d_rank_level_up_problems/c/accompanied_by_stdin/1.c
@@ -1,4 +1,5 @@
 // Paiza 問題集 Dランクレベルアップメニュー 入力された 2 つの文字列を出力 C編
+// https://paiza.jp/works/mondai/d_rank_level_up_problems/d_rank_level_up_problems__accompanied_by_stdin_1
 #include <stdio.h>
 
 #define LEN 10
