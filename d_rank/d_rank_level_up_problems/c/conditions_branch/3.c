03135637e54d88e0fc701ef386a91de1666ec544
kos-y <kosyos.address@gmail.com>
2025-04-18 22:52:42 +0900

    Paiza 問題集 Dランクレベルアップメニュー 数値演算結果で分岐 C編

--- d_rank/d_rank_level_up_problems/c/conditions_branch/3.c
+++ d_rank/d_rank_level_up_problems/c/conditions_branch/3.c
@@ -1,10 +1,19 @@
+// Paiza 問題集 Dランクレベルアップメニュー 数値演算結果で分岐 C編
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
     int a, b, c;
-
     scanf("%d %d %d", &a, &b, &c);
-    printf("%s\n", a * b <= c ? "YES" : "NO");
+
+    char x[4];
+    if (a * b <= c) {
+        strcpy(x, "YES");
+    } else {
+        strcpy(x, "NO");
+    }
+
+    printf("%s\n", x);
     return 0;
 }
