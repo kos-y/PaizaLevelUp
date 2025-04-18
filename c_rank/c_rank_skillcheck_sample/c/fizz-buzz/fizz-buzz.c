67918b3544eeecf330b4759cef20a40ba4469e22
kos-y <kosyos.address@gmail.com>
2025-04-18 19:52:03 +0900

    Paiza 問題集 Cランク・スキルチェック見本問題セット C

---
+++ c_rank/c_rank_skillcheck_sample/c/fizz-buzz/fizz-buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    int n;
+
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            printf("Fizz Buzz\n");
+        }
+        else if (i % 3 == 0) {
+            printf("Fizz\n");
+        }
+        else if (i % 5 == 0) {
+            printf("Buzz\n");
+        }
+        else {
+            printf("%d\n", i);
+        }
+    }
+    
+    return 0;
+}
