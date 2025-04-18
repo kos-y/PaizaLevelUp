8ec00f93df1d5354abb45900ad75e2498b1781a8
kos-y <kosyos.address@gmail.com>
2025-04-19 00:29:53 +0900

    // Paiza 問題集 標準出力メニュー 10 行以内の出力 C編（paizaランク D 相当）

--- c_rank/stdout_primer/c/n_line/step3.c
+++ c_rank/stdout_primer/c/n_line/step3.c
@@ -1,10 +1,12 @@
+// Paiza 問題集 標準出力メニュー 10 行以内の出力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__n_line_step3
 #include <stdio.h>
 
 int main()
 {
     int n;
+    
     scanf("%d", &n);
-
     for (int i = 1; i <= n; i++) {
         printf("%d\n", i);
     }
