b8790c4e171bbc9ea69657ecf20e333c9cd7b163
kos-y <kosyos.address@gmail.com>
2025-04-19 08:56:02 +0900

    Paiza 問題集 標準出力メニュー 複数の数値を半角スペース埋めで出力 C編（paizaランク D 相当）

--- c_rank/stdout_primer/c/print_width/step3.c
+++ c_rank/stdout_primer/c/print_width/step3.c
@@ -1,19 +1,18 @@
+// Paiza 問題集 標準出力メニュー 複数の数値を半角スペース埋めで出力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__print_width_step3
 #include <stdio.h>
 
 int main()
 {
     int n;
+
     scanf("%d", &n);
-
-    int *pm = (int*)malloc(sizeof(int)* n);
     for (int i = 0; i < n; i++) {
-        scanf("%d", &pm[i]);
+        int m;
+
+        scanf("%d", &m);
+        printf("%3d\n", m);
     }
 
-    for (int i = 0; i < n; i++) {
-        printf("%3d\n", pm[i]);
-    }
-
-    free(pm);
     return 0;
 }
