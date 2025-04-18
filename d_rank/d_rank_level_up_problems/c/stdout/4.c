a8b1cbbfa95db969b866cbfcf5636356630fd748
kos-y <kosyos.address@gmail.com>
2025-04-18 22:19:21 +0900

    Paiza 問題集 Dランクレベルアップメニュー 3 行の出力 C編

--- d_rank/d_rank_level_up_problems/c/stdout/4.c
+++ d_rank/d_rank_level_up_problems/c/stdout/4.c
@@ -1,9 +1,13 @@
+// Paiza 問題集 Dランクレベルアップメニュー 3 行の出力 C編
 #include <stdio.h>
+
+#define NUM_ELE(x) sizeof(x) / sizeof(x[0])
 
 int main()
 {
     int v[] = {8, 1, 3};
-    for (int i = 0; i < sizeof(v) / sizeof(v[0]); i++) {
+    
+    for (int i = 0; i < NUM_ELE(v); i++) {
         printf("%d\n", v[i]);
     }
 
