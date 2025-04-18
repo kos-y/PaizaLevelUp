0cd7dd908905f5b5eb3ba8d44412e4630cd0903a
kos-y <kosyos.address@gmail.com>
2025-04-18 21:56:57 +0900

    Paiza 問題集  Dランク・スキルチェック見本セット 掛け算 C編

--- d_rank/d_rank_skillcheck_sample/c/multiplication.c
+++ d_rank/d_rank_skillcheck_sample/c/multiplication.c
@@ -1,11 +1,15 @@
+// Paiza 問題集
+// Dランク・スキルチェック見本セット
+// 掛け算 C編
 #include <stdio.h>
 
 int main()
 {
     int a, b;
 
-    scanf("%d ", &a);
-    scanf("%d ", &b);
+    scanf("%d", &a);
+    scanf("%d", &b);
+
     printf("%d\n", a * b);
     return 0;
 }
