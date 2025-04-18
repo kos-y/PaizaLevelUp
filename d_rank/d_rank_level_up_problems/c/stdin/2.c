e63e9c73a906d1b905a81674bd10e969e2d64483
kos-y <kosyos.address@gmail.com>
2025-04-18 22:32:48 +0900

    Paiza 問題集 Dランクレベルアップメニュー 1 行の入力 C編

--- d_rank/d_rank_level_up_problems/c/stdin/2.c
+++ d_rank/d_rank_level_up_problems/c/stdin/2.c
@@ -1,11 +1,17 @@
+// Paiza 問題集 Dランクレベルアップメニュー 1 行の入力 C編
 #include <stdio.h>
+
+#define LEN 10
 
 int main()
 {
-    char s[11], t[11];
+    char s[LEN + 1], t[LEN + 1];
+    char format[5];
 
-    scanf("%s ", s);
-    scanf("%s ", t);
+    sprintf(format, "%%%ds", LEN);
+    scanf(format, s);
+    scanf(format, t);
+
     printf("%s\n", s);
     printf("%s\n", t);
     return 0;
