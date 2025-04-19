fe0e09656ee7c066dc4c997d8f6f0919240629ef
kos-y <kosyos.address@gmail.com>
2025-04-19 09:00:48 +0900

    【出力幅を指定して出力】N 個の数値を M けた半角スペース埋めで出力 C編（paizaランク D 相当）

--- c_rank/stdout_primer/c/print_width/boss.c
+++ c_rank/stdout_primer/c/print_width/boss.c
@@ -1,7 +1,6 @@
 // Paiza 問題集 標準出力メニュー 【出力幅を指定して出力】N 個の数値を M けた半角スペース埋めで出力 C編（paizaランク D 相当）
 // https://paiza.jp/works/mondai/stdout_primer/stdout_primer__print_width_boss
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
