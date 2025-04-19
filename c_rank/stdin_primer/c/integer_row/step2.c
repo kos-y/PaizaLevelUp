a55289d52aa83bc3b7c683929a64638a10fc1aee
kos-y <kosyos.address@gmail.com>
2025-04-19 09:46:12 +0900

    Paiza 問題集 標準入力メニュー 2 行の整数の入力 C編（paizaランク D 相当）

--- c_rank/stdin_primer/c/integer_row/step2.c
+++ c_rank/stdin_primer/c/integer_row/step2.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 標準入力メニュー 2 行の整数の入力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__integer_row_step2
 #include <stdio.h>
 
 int main()
@@ -6,6 +8,7 @@ int main()
 
     scanf("%d", &a);
     scanf("%d", &b);
+    
     printf("%d\n", a);
     printf("%d\n", b);
     return 0;
