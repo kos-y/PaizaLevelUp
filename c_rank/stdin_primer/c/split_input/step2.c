0adc40913d4941b9ff1443187050d844dadc40e7
kos-y <kosyos.address@gmail.com>
2025-04-19 09:31:37 +0900

    Paiza 問題集 標準入力メニュー 半角スペース区切りの 2 つの入力 C編（paizaランク D 相当）

--- c_rank/stdin_primer/c/split_input/step2.c
+++ c_rank/stdin_primer/c/split_input/step2.c
@@ -1,16 +1,16 @@
+// Paiza 問題集 標準入力メニュー 半角スペース区切りの 2 つの入力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__split_input_step2
 #include <stdio.h>
 #include <string.h>
 
 int main()
 {
-    char s[22];
-    fgets(s, sizeof(s), stdin);
-    s[strcspn(s, "\n")] = '\0';
+    for (int i = 0; i < 2; i++) {
+        char s[11];
 
-    char* p = strtok(s, " ");
-    while (p != NULL) {
-        printf("%s\n", p);
-        p = strtok(NULL, " ");
+        scanf("%10s", s);
+        printf("%s\n", s);
     }
+
     return 0;
 }
