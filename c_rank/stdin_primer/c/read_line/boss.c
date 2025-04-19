bbbbb85655e662817c6012f75675e41575c3d961
kos-y <kosyos.address@gmail.com>
2025-04-19 09:23:47 +0900

    Paiza 問題集 標準入力メニュー 【行入力】1,000 行の入力 C編（paizaランク D 相当）

--- c_rank/stdin_primer/c/read_line/boss.c
+++ c_rank/stdin_primer/c/read_line/boss.c
@@ -1,16 +1,20 @@
+// 【行入力】1,000 行の入力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__read_line_boss
 #include <stdio.h>
+
 #define N 1000
+#define LEN 10
 
 int main()
 {
-    char s[N][11];
+    char format[6];
 
+    sprintf(format, "%%%ds", LEN);
     for (int i = 0; i < N; i++) {
-        scanf("%10s", s[i]);
-    }
+        char s[LEN + 1];
 
-    for (int i = 0; i < N; i++) {
-        printf("%s\n", s[i]);
+        scanf(format, s);
+        printf("%s\n", s);
     }
 
     return 0;
