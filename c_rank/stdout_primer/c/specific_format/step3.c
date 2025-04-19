2ad29aad6439b5cd370dfd67c1617e9ae13c8c1b
kos-y <kosyos.address@gmail.com>
2025-04-19 09:06:08 +0900

    Paiza 問題集 標準出力メニュー 九九表を罫線入りで出力 C編（paizaランク C 相当）

--- c_rank/stdout_primer/c/specific_format/step3.c
+++ c_rank/stdout_primer/c/specific_format/step3.c
@@ -1,4 +1,7 @@
+// Paiza 問題集 標準出力メニュー 九九表を罫線入りで出力 C編（paizaランク C 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__specific_format_step3
 #include <stdio.h>
+
 #define N 9
 
 int main()
@@ -10,6 +13,7 @@ int main()
                 printf(" | ");
             }
         }
+        
         printf("\n");
         if (i < N) {
             printf("==========================================\n");
