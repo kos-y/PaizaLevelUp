79b0e4798978421dde326cf7bcda60975817f704
kos-y <kosyos.address@gmail.com>
2025-04-19 09:12:36 +0900

    Paiza 問題集 標準出力メニュー 1 行の入力 C編（paizaランク D 相当）

--- c_rank/stdin_primer/c/read_line/step1.c
+++ c_rank/stdin_primer/c/read_line/step1.c
@@ -1,10 +1,16 @@
+// Paiza 問題集 標準出力メニュー 1 行の入力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__read_line_step1
 #include <stdio.h>
+#include <string.h>
+
+#define LEN 10
 
 int main()
 {
-    char s[11];
+    char s[LEN + 2];
 
     fgets(s, sizeof(s), stdin);
+    s[strcspn(s, "\n")] = '\0';
     printf("%s", s);
     return 0;
 }
