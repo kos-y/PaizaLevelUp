bcefa1a60c7e1eec005af16e040a0c0797b2407a
kos-y <kosyos.address@gmail.com>
2025-04-19 09:48:27 +0900

    Paiza 問題集 標準入力メニュー 5 行の整数の入力 C編（paizaランク D 相当）

--- c_rank/stdin_primer/c/integer_row/step3.c
+++ c_rank/stdin_primer/c/integer_row/step3.c
@@ -1,15 +1,15 @@
+// Paiza 問題集 標準入力メニュー 5 行の整数の入力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__integer_row_step3
 #include <stdio.h>
-#define N 5
 
 int main()
 {
-    int a[N];
+    for (int i = 0; i < 5; i++) {
+        int a;
 
-    for (int i = 0; i < N; i++) {
-        scanf("%d", &a[i]);
+        scanf("%d", &a);
+        printf("%d\n", a);
     }
-    for (int i = 0; i < N; i++) {
-        printf("%d\n", a[i]);
-    }
+
     return 0;
 }
