0869c633e18f4c6be33d78c351291a4d654e8afd
kos-y <kosyos.address@gmail.com>
2025-04-19 09:59:34 +0900

    Paiza 問題集 標準入力メニュー 【整数の半角スペース区切りの入力】1,000個の整数の半角スペース区切りの入力 C編（paizaランク D 相当）

--- c_rank/stdin_primer/c/integer_space/boss.c
+++ c_rank/stdin_primer/c/integer_space/boss.c
@@ -1,15 +1,15 @@
+// Paiza 問題集 標準入力メニュー 【整数の半角スペース区切りの入力】1,000個の整数の半角スペース区切りの入力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__integer_space_boss
 #include <stdio.h>
-#define N 1000
 
 int main()
 {
-    int a[N];
+    for (int i = 0; i < 1000; i++) {
+        int a;
 
-    for (int i = 0; i < N; i++) {
-        scanf("%d", &a[i]);
+        scanf("%d", &a);
+        printf("%d\n", a);
     }
-    for (int i = 0; i < N; i++) {
-        printf("%d\n", a[i]);
-    }
+
     return 0;
 }
