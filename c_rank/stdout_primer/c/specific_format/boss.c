9bb871519415dcd74a755cd23797b6465eab8fb2
kos-y <kosyos.address@gmail.com>
2025-04-19 09:08:52 +0900

    Paiza 問題集 標準出力メニュー 【出力形式を指定して出力】ペアの数値の入った表を罫線入りで出力 2 C編（paizaランク C 相当）

--- c_rank/stdout_primer/c/specific_format/boss.c
+++ c_rank/stdout_primer/c/specific_format/boss.c
@@ -1,4 +1,7 @@
+// Paiza 問題集 標準出力メニュー 【出力形式を指定して出力】ペアの数値の入った表を罫線入りで出力 2 C編（paizaランク C 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__specific_format_boss
 #include <stdio.h>
+
 #define DATA_WIDTH 22
 #define SPLIT_WIDTH 3
 
@@ -7,9 +10,7 @@ int main()
     int h, w, a, b;
     
     scanf("%d %d %d %d", &h, &w, &a, &b);
-    
     int l = (w * DATA_WIDTH) + ((w - 1) * SPLIT_WIDTH);
-    
     for (int i = 0; i < h; i++) {
         for (int j = 0; j < w; j++) {
             printf("(%9d, %9d)", a, b);
@@ -17,12 +18,13 @@ int main()
                 printf(" | ");
             }
         }
+
         printf("\n");
-        
         if (i < h - 1) {
             for (int j = 0; j < l; j++) {
                 printf("=");
             }
+            
             printf("\n");
         }
     }
