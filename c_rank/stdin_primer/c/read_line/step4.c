fd7f10814bbab56e518907eefc02bb3bd82f9956
kos-y <kosyos.address@gmail.com>
2025-04-19 09:21:13 +0900

    Paiza 問題集 標準出力メニュー 10 行の入力 C編（paizaランク D 相当）

--- c_rank/stdin_primer/c/read_line/step4.c
+++ c_rank/stdin_primer/c/read_line/step4.c
@@ -1,16 +1,20 @@
+// Paiza 問題集 標準出力メニュー 10 行の入力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__read_line_step4
 #include <stdio.h>
+
 #define N 10
+#define LEN 10
 
 int main()
 {
-    char s[N][11];
+    char format[5];
 
+    sprintf(format, "%%%ds", LEN);
     for (int i = 0; i < N; i++) {
-        scanf("%10s", s[i]);
-    }
+        char s[LEN + 1];
 
-    for (int i = 0; i < N; i++) {
-        printf("%s\n", s[i]);
+        scanf(format, s);
+        printf("%s\n", s);
     }
 
     return 0;
