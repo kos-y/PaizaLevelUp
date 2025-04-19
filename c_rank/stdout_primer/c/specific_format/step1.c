107945734f136e3330b039f930636e902f26fc60
kos-y <kosyos.address@gmail.com>
2025-04-19 09:03:19 +0900

    Paiza 問題集 標準出力メニュー 2 つの文字列を出力 C編（paizaランク D 相当）

--- c_rank/stdout_primer/c/specific_format/step1.c
+++ c_rank/stdout_primer/c/specific_format/step1.c
@@ -1,11 +1,17 @@
+// Paiza 問題集 標準出力メニュー 2 つの文字列を出力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__specific_format_step1
 #include <stdio.h>
+
+#define LEN 100
 
 int main()
 {
-    char s[101], t[101];
+    char s[LEN + 1], t[LEN + 1];
+    char format[6];
 
-    scanf("%s", s);
-    scanf("%s", t);
+    sprintf(format, "%%%ds", LEN);
+    scanf(format, s);
+    scanf(format, t);
     printf("%s + %s = %s%s\n", s, t, s, t);
     return 0;
 }
