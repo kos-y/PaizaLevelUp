7ea9ab9c6fb9c9f519b42d235bd59c872ccb0fb2
kos-y <kosyos.address@gmail.com>
2025-04-19 09:57:40 +0900

    Paiza 問題集 標準入力メニュー 10 個の整数の半角スペース区切りの入力 C編（paizaランク D 相当）

--- c_rank/stdin_primer/c/integer_space/step4.c
+++ c_rank/stdin_primer/c/integer_space/step4.c
@@ -1,15 +1,15 @@
+// Paiza 問題集 標準入力メニュー 10 個の整数の半角スペース区切りの入力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__integer_space_step4
 #include <stdio.h>
-#define N 10
 
 int main()
 {
-    int a[N];
+    for (int i = 0; i < 10; i++) {
+        int a;
 
-    for (int i = 0; i < N; i++) {
-        scanf("%d", &a[i]);
+        scanf("%d", &a);
+        printf("%d\n", a);
     }
-    for (int i = 0; i < N; i++) {
-        printf("%d\n", a[i]);
-    }
+
     return 0;
 }
