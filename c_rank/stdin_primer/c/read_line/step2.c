b84c5e2c5b36fabd0ab6765e72a1e65e64b3fb14
kos-y <kosyos.address@gmail.com>
2025-04-19 09:14:53 +0900

    Paiza 問題集 標準出力メニュー 2 行の入力 C編（paizaランク D 相当）

--- c_rank/stdin_primer/c/read_line/step2.c
+++ c_rank/stdin_primer/c/read_line/step2.c
@@ -1,11 +1,18 @@
+// Paiza 問題集 標準出力メニュー 2 行の入力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__read_line_step2
 #include <stdio.h>
+
+#define LEN 10
 
 int main()
 {
-    char s[11], t[11];
+    char s[LEN + 1], t[LEN + 1];
+    char format[5];
 
-    scanf("%10s", s);
-    scanf("%10s", t);
+    sprintf(format, "%%%ds", LEN);
+    scanf(format, s);
+    scanf(format, t);
+    
     printf("%s\n", s);
     printf("%s\n", t);
     return 0;
