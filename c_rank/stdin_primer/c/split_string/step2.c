e4c36bbfd9557a3f7993e7de134a29d9a4a2e043
kos-y <kosyos.address@gmail.com>
2025-04-19 09:26:58 +0900

    Paiza 問題集 標準入力メニュー 3 つの文字列の半角スペース区切りでの分割 C編（paizaランク D 相当）

--- c_rank/stdin_primer/c/split_string/step2.c
+++ c_rank/stdin_primer/c/split_string/step2.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 標準入力メニュー 3 つの文字列の半角スペース区切りでの分割 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__split_string_step2
 #include <stdio.h>
 #include <string.h>
 
