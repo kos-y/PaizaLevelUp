b0cdb83fbe78d3c3dfcb3917ea1d00ac0cf417cf
kos-y <kosyos.address@gmail.com>
2025-04-21 22:01:12 +0900

    Paiza 問題集 2 行目で与えられる N 個の実数の入力 (large) C編（paizaランク D 相当）

--- c_rank/stdin_primer/c/real_number/step4.c
+++ c_rank/stdin_primer/c/real_number/step4.c
@@ -1,47 +1,17 @@
+// Paiza 問題集 2 行目で与えられる N 個の実数の入力 (large) C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__real_number_step4
 #include <stdio.h>
-#include <stdlib.h>
-#define LEN 10
-
-struct S {
-    char s[LEN + 1];
-    struct S* p_next;
-};
 
 int main()
 {
     int n;
+
     scanf("%d", &n);
-
-    char f[5];
-    sprintf(f, "%%%ds", LEN);
-
-    struct S* p_start = NULL;
-    struct S* p_current = NULL;
     for (int i = 0; i < n; i++) {
-        struct S* p = (struct S*)calloc(1, sizeof(struct S));
-        if (i == 0) {
-            p_start = p;
-            p_current = p;
-        }
-        else {
-            p_current->p_next = p;
-            p_current = p;
-        }
+        double a;
 
-        scanf(f, p_current->s);
-    }
-
-    p_current = p_start;
-    while (p_current != NULL) {
-        printf("%s\n", p_current->s);
-        p_current = p_current->p_next;
-    }
-
-    p_current = p_start;
-    while (p_current != NULL) {
-        struct S* p = p_current->p_next;
-        free(p_current);
-        p_current = p;
+        scanf("%lf", &a);
+        printf("%lf\n", a);
     }
 
     return 0;
