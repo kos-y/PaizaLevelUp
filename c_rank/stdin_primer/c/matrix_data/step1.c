e0c75b98a2cee769b27af6f18066059e07b66016
kos-y <kosyos.address@gmail.com>
2025-04-21 22:14:45 +0900

    Paiza 問題集 3 行 3 列の整数の入力 C編（paizaランク D 相当）

--- c_rank/stdin_primer/c/matrix_data/step1.c
+++ c_rank/stdin_primer/c/matrix_data/step1.c
@@ -1,23 +1,22 @@
+// Paiza 問題集 3 行 3 列の整数の入力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__matrix_data_step1
 #include <stdio.h>
+
 #define N 3
 
 int main()
 {
-    int a[N][N];
-
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            scanf("%d", &a[i][j]);
-        }
-    }
+            int a;
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            printf("%d", a[i][j]);
+            scanf("%d", &a);
+            printf("%d", a);
             if (j < N - 1) {
                 printf(" ");
             }
         }
+
         printf("\n");
     }
 
