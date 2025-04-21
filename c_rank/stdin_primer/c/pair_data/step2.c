b8f99210456c43f1e568f72cf115c918a285fc92
kos-y <kosyos.address@gmail.com>
2025-04-21 22:07:00 +0900

    Paiza 問題集 N 組の整数の入力 C編（paizaランク D 相当）

--- c_rank/stdin_primer/c/pair_data/step2.c
+++ c_rank/stdin_primer/c/pair_data/step2.c
@@ -1,44 +1,17 @@
+// Paiza 問題集 N 組の整数の入力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__pair_data_step2
 #include <stdio.h>
-#include <stdlib.h>
-
-struct S {
-    int a;
-    int b;
-
-    struct S* next;
-};
 
 int main()
 {
     int n;
+
     scanf("%d", &n);
-
-    struct S* ps = NULL;
-    struct S* pc = NULL;
     for (int i = 0; i < n; i++) {
-        struct S* p = (struct S*)calloc(1, sizeof(struct S));
-        if (i == 0) {
-            ps = p;
-            pc = p;
-        }
-        else {
-            pc->next = p;
-            pc = p;
-        }
-    }
+        int a, b;
 
-    for (pc = ps; pc != NULL; pc = pc->next) {
-        scanf("%d %d", &pc->a, &pc->b);
-    }
-
-    for (pc = ps; pc != NULL; pc = pc->next) {
-        printf("%d %d\n", pc->a, pc->b);
-    }
-
-    for (pc = ps; pc != NULL; ) {
-        struct S* p = pc->next;
-        free(pc);
-        pc = p;
+        scanf("%d %d", &a, &b);
+        printf("%d %d\n", a, b);
     }
 
     return 0;
