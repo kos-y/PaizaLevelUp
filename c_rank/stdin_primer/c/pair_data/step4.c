16e3fce3f680998da50858f97f152028eeaf4e35
kos-y <kosyos.address@gmail.com>
2025-04-21 22:12:26 +0900

    Paiza 問題集 文字列と整数の組の入力 C編（paizaランク D 相当）

--- c_rank/stdin_primer/c/pair_data/step4.c
+++ c_rank/stdin_primer/c/pair_data/step4.c
@@ -1,46 +1,20 @@
+// Paiza 問題集 文字列と整数の組の入力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__pair_data_step4
 #include <stdio.h>
-#include <stdlib.h>
+
 #define LEN 10
-
-struct S {
-    char s[LEN + 1];
-    int a;
-
-    struct S* next;
-};
 
 int main()
 {
     int n;
+
     scanf("%d", &n);
-
-    struct S* ps = NULL;
-    struct S* pc = NULL;
     for (int i = 0; i < n; i++) {
-        struct S* p = (struct S*)calloc(1, sizeof(struct S));
-        if (i == 0) {
-            ps = p;
-            pc = p;
-        }
-        else {
-            pc->next = p;
-            pc = p;
-        }
-    }
+        char s[LEN + 1];
+        int a;
 
-    for (pc = ps; pc != NULL; pc = pc->next) {
-        scanf("%s %d", pc->s, &pc->a);
-    }
-
-    pc = ps;
-    for (int i = 1; pc != NULL; pc = pc->next, i++) {
-        printf("%s %d\n", pc->s, pc->a);
-    }
-
-    for (pc = ps; pc != NULL; ) {
-        struct S* p = pc->next;
-        free(pc);
-        pc = p;
+        scanf("%s %d", s, &a);
+        printf("%s %d\n", s, a);
     }
 
     return 0;
