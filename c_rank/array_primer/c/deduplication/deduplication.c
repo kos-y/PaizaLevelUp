722799829c6b1388a6d4e73c4ffe28751197aba6
kos-y <kosyos.address@gmail.com>
2025-04-27 19:03:52 +0900

    Paiza 問題集 配列メニュー 重複の削除 C編（paizaランク D 相当）

--- c_rank/array_primer/c/deduplication/deduplication.c
+++ c_rank/array_primer/c/deduplication/deduplication.c
@@ -1,18 +1,19 @@
+// Paiza 問題集 配列メニュー 重複の削除 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_primer/array_primer__deduplication
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NELEMS(x) (sizeof(x) / sizeof(x[0]))
+#define NUM_ELE(x) (sizeof(x) / sizeof(x[0]))
 
 int cmp(const void* a, const void* b);
 
 int main()
 {
     int a[] = {1, 3, 5, 1, 2, 3, 6, 6, 5, 1, 4};
-    
-    qsort(a, NELEMS(a), sizeof(int), cmp);
-    
     int prev = -1;
-    for (int i = 0; i < NELEMS(a); i++) {
+    
+    qsort(a, NUM_ELE(a), sizeof(int), cmp);
+    for (int i = 0; i < NUM_ELE(a); i++) {
         if (prev != a[i]) {
             printf("%d\n", a[i]);
             prev = a[i];
