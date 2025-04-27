f90eb00b28ae0e4eb840c66a0f3a8e6f991ab099
kos-y <kosyos.address@gmail.com>
2025-04-27 18:49:01 +0900

    Paiza 問題集 配列メニュー 全要素の出力 C編（paizaランク D 相当）

--- c_rank/array_primer/c/string_output/step2.c
+++ c_rank/array_primer/c/string_output/step2.c
@@ -1,12 +1,15 @@
+// Paiza 問題集 配列メニュー 全要素の出力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_primer/array_primer__string_output_step2
 #include <stdio.h>
-#define NELEMS(x) sizeof(x) / sizeof(x[0])
+#define NUM_ELE(x) sizeof(x) / sizeof(x[0])
 
 int main()
 {
     char* sz[] = { "good", "morning", "paiza", "813", "pa13" };
 
-    for (int i = 0; i < NELEMS(sz); i++) {
+    for (int i = 0; i < NUM_ELE(sz); i++) {
         printf("%s\n", sz[i]);
     }    
+    
     return 0;
 }
