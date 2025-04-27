e14a0f00f2477175cdf45ca23de8843c2456c90a
kos-y <kosyos.address@gmail.com>
2025-04-27 18:51:24 +0900

    Paiza 問題集 配列メニュー 文字列の配列の入力 1 C編（paizaランク D 相当）

--- c_rank/array_primer/c/string_input/step1.c
+++ c_rank/array_primer/c/string_input/step1.c
@@ -1,12 +1,15 @@
+// Paiza 問題集 配列メニュー 文字列の配列の入力 1 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_primer/array_primer__string_input_step1
 #include <stdio.h>
-#define NELEMS(x) sizeof(x) / sizeof(x[0])
+#define NUM_ELE(x) sizeof(x) / sizeof(x[0])
 
 int main()
 {
     char* sz[] = { "eight", "one", "three", "paiza", "pa13", "813" };
 
-    for (int i = 0; i < NELEMS(sz); i++) {
+    for (int i = 0; i < NUM_ELE(sz); i++) {
         printf("%s\n", sz[i]);
-    }    
+    }
+    
     return 0;
 }
