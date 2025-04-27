04129072a1d1e0b7d0e11f98f42a469a35615b20
kos-y <kosyos.address@gmail.com>
2025-04-27 17:51:33 +0900

    Paiza 問題集 配列メニュー 配列に含まれている? 1 C編（paizaランク D 相当）

--- c_rank/array_primer/c/search_include/step1.c
+++ c_rank/array_primer/c/search_include/step1.c
@@ -1,13 +1,15 @@
+// Paiza 問題集 配列メニュー 配列に含まれている? 1 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_primer/array_primer__search_include_step1
 #include <stdio.h>
 #include <stdbool.h>
-#define NELEMS(x) sizeof(x) / sizeof(x[0])
+#define NUM_ELE(x) sizeof(x) / sizeof(x[0])
 
 int main()
 {
     int a[] = {10, 13, 21, 1, 6, 51, 10, 8, 15, 6};
     bool find = false;
 
-    for (int i = 0; i < NELEMS(a); i++) {
+    for (int i = 0; i < NUM_ELE(a); i++) {
         if (a[i] == 6) {
             find = true;
             break;
