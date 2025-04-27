dff28bf6a27d98a3fae32c360072959abc58906e
kos-y <kosyos.address@gmail.com>
2025-04-27 15:21:37 +0900

    Paiza 問題集 データセット選択メニュー 最大値と最小値 C編（paizaランク D 相当）

--- c_rank/data_structure/c/array/step3.c
+++ c_rank/data_structure/c/array/step3.c
@@ -1,22 +1,23 @@
+// Paiza 問題集 データセット選択メニュー 最大値と最小値 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/data_structure/data_structure__array_step3
 #include <stdio.h>
 #include <limits.h>
 #define N 3
 
 int main()
 {
-    int vs[N];
-    for (int i = 0; i < N; i++) {
-        scanf("%d", &vs[i]);
-    }
-
     int min = INT_MAX;
     int max = INT_MIN;
     for (int i = 0; i < N; i++) {
-        if (vs[i] < min) {
-            min = vs[i];
+        int n;
+
+        scanf("%d", &n);
+        if (n < min) {
+            min = n;
         }
-        if (max < vs[i]) {
-            max = vs[i];
+
+        if (max < n) {
+            max = n;
         }
     }
 
