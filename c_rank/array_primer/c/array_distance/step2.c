d5ef0b82309ef9b2a29b80e913461e8cb9c05d3f
kos-y <kosyos.address@gmail.com>
2025-04-27 19:45:52 +0900

    Paiza 問題集 配列メニュー 二点間の距離 2 C編（paizaランク D 相当）

--- c_rank/array_primer/c/array_distance/step2.c
+++ c_rank/array_primer/c/array_distance/step2.c
@@ -1,21 +1,20 @@
+// Paiza 問題集 配列メニュー 二点間の距離 2 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_primer/array_primer__array_distance_step2
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main()
 {
     int n, a, b;
-    scanf("%d %d %d ", &n, &a, &b);
 
+    scanf("%d %d %d ", &n, &a, &b);
     int* x = (int*)calloc(n, sizeof(int));
     int* y = (int*)calloc(n, sizeof(int));
-
     for (int i = 0; i < n; i++) {
-        scanf("%d  %d ", &x[i], &y[i]);
+        scanf("%d %d", &x[i], &y[i]);
     }
     
     printf("%d", abs(x[a - 1] - x[b - 1]) + abs(y[a - 1] - y[b - 1]));
-
     free(x);
     free(y);
     return 0;
