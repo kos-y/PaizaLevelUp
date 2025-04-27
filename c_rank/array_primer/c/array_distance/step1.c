f8c2ff4ef56dac30539868287e25fbe5f98c80f3
kos-y <kosyos.address@gmail.com>
2025-04-27 19:44:41 +0900

    Paiza 問題集 配列メニュー 二点間の距離 1 C編（paizaランク D 相当）

--- c_rank/array_primer/c/array_distance/step1.c
+++ c_rank/array_primer/c/array_distance/step1.c
@@ -1,14 +1,16 @@
+// Paiza 問題集 配列メニュー 二点間の距離 1 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_primer/array_primer__array_distance_step1
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main()
 {
     int n;
+
     scanf("%d ", &n);
-    
     for (int i = 0; i < n; i++) {
         int x, y;
+
         scanf("%d  %d ", &x, &y);
         printf("%d\n", abs(2 - x) + abs(3 - y));
     }
