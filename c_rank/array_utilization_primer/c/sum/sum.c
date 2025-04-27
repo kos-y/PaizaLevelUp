b1057ecf0715ba640a79c0d59f9a50ef55ead408
kos-y <kosyos.address@gmail.com>
2025-04-27 22:36:38 +0900

    Paiza 問題集 配列活用メニュー 全ての要素の和 C編（paizaランク D 相当）

--- c_rank/array_utilization_primer/c/sum/sum.c
+++ c_rank/array_utilization_primer/c/sum/sum.c
@@ -1,38 +1,20 @@
+// Paiza 問題集 配列活用メニュー 全ての要素の和 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__sum
 #include <stdio.h>
 #include <stdlib.h>
 
-int* input(int n);
-int sum(int* a, int n);
-
 int main()
 {
-    int n;
+    int n, sum = 0;
 
     scanf("%d", &n);
-    int* a = input(n);
-    printf("%d\n", sum(a, n));
-    free(a);
+    for (int i = 0; i < n; i++) {
+        int a;
+
+        scanf("%d", &a);
+        sum += a;
+    }
+
+    printf("%d\n", sum);
     return 0;
-}
-
-int* input(int n)
-{
-    int* a = (int*)malloc(sizeof(int) * n);
-
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &a[i]);
-    }
-
-    return a;
-}
-
-int sum(int* a, int n)
-{
-    int s = 0;
-
-    for (int i = 0; i < n; i++) {
-        s += a[i];
-    }
-
-    return s;
 }
