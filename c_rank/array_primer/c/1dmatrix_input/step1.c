cd53c1166cb6ee69b6ef3737ceb4b0ebc67c3762
kos-y <kosyos.address@gmail.com>
2025-04-27 17:18:29 +0900

    Paiza 問題集 配列メニュー 配列の入力 1 C編（paizaランク D 相当）

--- c_rank/array_primer/c/1dmatrix_input/step1.c
+++ c_rank/array_primer/c/1dmatrix_input/step1.c
@@ -1,15 +1,16 @@
+// Paiza 問題集 配列メニュー 配列の入力 1 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_primer/array_primer__1dmatrix_input_step1
 #include <stdio.h>
 #include <string.h>
+#define NUM_ELE(x) sizeof(x) / sizeof(x[0])
 
 int main()
 {
-    char s[] = "8 1 3 3 8 1 1 3 8 8";
-    int i = 0;
+    int n[] = {8, 1, 3, 3, 8, 1, 1, 3, 8, 8};
 
-    char* p = strtok(s, " ");
-    while (p != NULL) {
-        printf("%s\n", p);
-        p = strtok(NULL, " ");
+    for (int i = 0; i < NUM_ELE(n); i++) {
+        printf("%d\n", n[i]);
     }
+
     return 0;
 }
