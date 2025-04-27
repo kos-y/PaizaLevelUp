d73596db1290950e2f9064667be285c3a214a734
kos-y <kosyos.address@gmail.com>
2025-04-27 17:01:30 +0900

    Paiza 問題集 文字列処理メニュー 数式の計算（ 1 桁） C編（paizaランク C 相当）

--- c_rank/string_primer/c/advance/step8.c
+++ c_rank/string_primer/c/advance/step8.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 文字列処理メニュー 数式の計算（ 1 桁） C編（paizaランク C 相当）
+// https://paiza.jp/works/mondai/string_primer/advance_step8
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
@@ -8,12 +10,12 @@ int main()
 
     fgets(s, sizeof(s), stdin);
     s[strcspn(s, "\n")] = '\0';
-    
     int sum = s[0] - '0';
     char o = s[1];
     for (int i = 2; s[i] != '\0'; i++) {
         if (isdigit(s[i])) {
             int n = s[i] - '0';
+
             switch (o) {
                 case '+':
                     sum += n;
