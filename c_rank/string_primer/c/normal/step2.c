299cd96aef6af96ef5639826f992a07f0f499051
kos-y <kosyos.address@gmail.com>
2025-04-27 16:36:45 +0900

    Paiza 問題集 文字列処理メニュー 文字列の挿入 C編（paizaランク D 相当）

--- c_rank/string_primer/c/normal/step2.c
+++ c_rank/string_primer/c/normal/step2.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 文字列処理メニュー 文字列の挿入 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/string_primer/normal_step2
 #include <stdio.h>
 #include <string.h>
 
@@ -6,22 +8,22 @@
 int main()
 {
     char s[LEN + 2], t[LEN + 2];
+    int n, i;
+
     fgets(s, sizeof(s), stdin);
     s[strcspn(s, "\n")] = '\0';
     fgets(t, sizeof(t), stdin);
     t[strcspn(t, "\n")] = '\0';
-    
-    int n;
     scanf("%d", &n);
-    
-    int i;
     for (i = 0; i < n; i++) {
         printf("%c", s[i]);
     }
+
     printf("%s", t);
     for (; s[i] != '\0'; i++) {
         printf("%c", s[i]);
     }
+    
     printf("\n");
     return 0;
 }
