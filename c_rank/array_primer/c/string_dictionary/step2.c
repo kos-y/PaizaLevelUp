705d2dcef7ff1defb2099d564ae9333f40fee7f0
kos-y <kosyos.address@gmail.com>
2025-04-27 19:00:37 +0900

    Paiza 問題集 配列メニュー 辞書順に出力 2 C編（paizaランク D 相当）

--- c_rank/array_primer/c/string_dictionary/step2.c
+++ c_rank/array_primer/c/string_dictionary/step2.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 配列メニュー 辞書順に出力 2 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_primer/array_primer__string_dictionary_step2
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,10 +16,12 @@ int main()
     for (int i = 0; i < N; i++) {
         scanf("%s", s[i]);
     }
+
     qsort(s, N, M, cmp);
     for(int i = 0; i < N; i++) {
         printf("%s\n", s[i]);
     }
+    
     return 0;
 }
 
