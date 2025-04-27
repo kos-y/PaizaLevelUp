032f21de8b0524bcd6db90fabce659cd467ef5b5
kos-y <kosyos.address@gmail.com>
2025-04-27 16:48:52 +0900

    Paiza 問題集 文字列処理メニュー 文字列の分割 C編（paizaランク C 相当）

--- c_rank/string_primer/c/advance/step1.c
+++ c_rank/string_primer/c/advance/step1.c
@@ -1,12 +1,14 @@
+// Paiza 問題集 文字列処理メニュー 文字列の分割 C編（paizaランク C 相当）
+// https://paiza.jp/works/mondai/string_primer/advance_step1
 #include <stdio.h>
 #include <string.h>
 
 int main()
 {
     char s[102];
+
     fgets(s, sizeof(s), stdin);
     s[strcspn(s, "\n")] = '\0';
-    
     char* p = strtok(s, ",");
     while (p != NULL) {
         printf("%s\n", p);
