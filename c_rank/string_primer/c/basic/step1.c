0f56a59ca9615f2a168c11868553fdb4cc6bfc29
kos-y <kosyos.address@gmail.com>
2025-04-27 16:25:28 +0900

    Paiza 問題集 文字列処理メニュー 文字列の出力 C編（paizaランク D 相当）

--- c_rank/string_primer/c/basic/step1.c
+++ c_rank/string_primer/c/basic/step1.c
@@ -1,7 +1,9 @@
+// Paiza 問題集 文字列処理メニュー 文字列の出力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/string_primer/basic_step1
 #include <stdio.h>
 
 int main()
 {
-    printf("paiza");
+    printf("paiza\n");
     return 0;
 }
