e06fadc395637f0dfd01e1f0ecfe853fca2cdd5c
kos-y <kosyos.address@gmail.com>
2025-04-27 18:29:39 +0900

    Paiza 問題集 配列メニュー 部分配列 C編（paizaランク D 相当）

--- c_rank/array_primer/c/partical_array/partical_array.c
+++ c_rank/array_primer/c/partical_array/partical_array.c
@@ -1,20 +1,20 @@
+// Paiza 問題集 配列メニュー 部分配列 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_primer/array_primer__partical_array
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
     int a, b, n;
 
     scanf("%d %d %d", &a, &b, &n);
-    int* pa = (int*)malloc(sizeof(int) * n);
     for (int i = 0; i < n; i++) {
-        scanf("%d", &pa[i]);
+        int v;
+
+        scanf("%d", &v);
+        if (a - 1 <= i && i < b) {
+            printf("%d\n", v);
+        }
     }
 
-    for (int i = a - 1; i < b; i++) {
-        printf("%d\n", pa[i]);
-    }
-
-    free(pa);
     return 0;
 }
