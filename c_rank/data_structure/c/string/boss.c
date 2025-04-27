61179fd8b50663f7f4e7259c50b7ffc60ab4e656
kos-y <kosyos.address@gmail.com>
2025-04-27 15:27:43 +0900

    Paiza 問題集 データセット選択メニュー 文字列の配列 C編（paizaランク C 相当）

--- c_rank/data_structure/c/string/boss.c
+++ c_rank/data_structure/c/string/boss.c
@@ -1,21 +1,17 @@
+// Paiza 問題集 データセット選択メニュー 文字列の配列 C編（paizaランク C 相当）
+// https://paiza.jp/works/mondai/data_structure/data_structure__string_boss
 #include <stdio.h>
 
 int main()
 {
     int h, w, r, c;
-    scanf("%d %d %d %d", &h, &w, &r, &c);
-    
     char s[100][101];
+
+    scanf("%d %d %d %d", &h, &w, &r, &c);
     for (int i = 0; i < h; i++) {
         scanf("%s", s[i]);
     }
-    
-    if (s[r - 1][c - 1] == '#') {
-        printf("Yes\n");
-    }
-    else {
-        printf("No\n");
-    }
-    
+
+    printf("%s\n", s[r - 1][c - 1] == '#' ? "Yes" : "No");
     return 0;
 }
