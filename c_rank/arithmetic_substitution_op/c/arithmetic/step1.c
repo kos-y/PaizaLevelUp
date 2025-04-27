aefd09120f45ff60dfe6591e93f0de3a4d7186ac
kos-y <kosyos.address@gmail.com>
2025-04-27 16:03:01 +0900

    Paiza 問題集 算術・代入演算メニュー 足し算 C編（paizaランク D 相当）

--- c_rank/arithmetic_substitution_op/c/arithmetic/step1.c
+++ c_rank/arithmetic_substitution_op/c/arithmetic/step1.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 算術・代入演算メニュー 足し算 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/arithmetic_substitution_op/arithmetic_substitution_op__arithmetic_step1
 #include <stdio.h>
 
 int main()
