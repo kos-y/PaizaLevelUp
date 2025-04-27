d036083e488bde69a6236dca5b6e0b3f5e55c6f8
kos-y <kosyos.address@gmail.com>
2025-04-27 15:25:04 +0900

    Paiza 問題集 データセット選択メニュー とても大きな数値の入力 C編（paizaランク C 相当）

--- c_rank/data_structure/c/string/step1.c
+++ c_rank/data_structure/c/string/step1.c
@@ -1,8 +1,10 @@
+// Paiza 問題集 データセット選択メニュー とても大きな数値の入力 C編（paizaランク C 相当）
+// https://paiza.jp/works/mondai/data_structure/data_structure__string_step1
 #include <stdio.h>
 
 int main()
 {
-    char n[10000];
+    char n[10001];
     
     scanf("%s", n);
     printf("%s\n", n);
