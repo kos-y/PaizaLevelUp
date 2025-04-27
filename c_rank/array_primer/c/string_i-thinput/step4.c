9e114f4ce174c83865e9dec7fe7c29a9bf2965a3
kos-y <kosyos.address@gmail.com>
2025-04-27 18:57:35 +0900

    Paiza 問題集 配列メニュー i 番目の出力 4 C編（paizaランク D 相当）

--- c_rank/array_primer/c/string_i-thinput/step4.c
+++ c_rank/array_primer/c/string_i-thinput/step4.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 配列メニュー i 番目の出力 4 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_primer/array_primer__string_i-thinput_step4
 #include <stdio.h>
 #include <string.h>
 
@@ -15,6 +17,7 @@ int main()
             printf("%c\n", p[l - 1]);
             break;
         }
+        
         p = strtok(NULL, " ");
     }
     return 0;
