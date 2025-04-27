7ba2c08ad401b0acf9020893aac51d3b2a77fe48
kos-y <kosyos.address@gmail.com>
2025-04-27 22:54:31 +0900

    Paiza 問題集 配列活用メニュー 【配列を参照する操作】全ての要素に対する操作 C編（paizaランク D 相当）

--- c_rank/array_utilization_primer/c/map/map.c
+++ c_rank/array_utilization_primer/c/map/map.c
@@ -1,43 +1,18 @@
+// Paiza 問題集 配列活用メニュー 【配列を参照する操作】全ての要素に対する操作 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__map
 #include <stdio.h>
-#include <stdlib.h>
-
-int* input(int n);
-void mapadd(int* a, int n, int k);
-void output(int* a, int n);
 
 int main()
 {
     int n, k;
 
     scanf("%d %d", &n, &k);
-    int* a = input(n);
-    mapadd(a, n, k);
-    output(a, n);
-    free(a);
+    for (int i = 0; i < n; i++) {
+        int a;
+
+        scanf("%d", &a);
+        printf("%d\n", a + k);
+    }
+
     return 0;
-}
-
-int* input(int n)
-{
-    int* a = (int*)malloc(sizeof(int) * n);
-
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &a[i]);
-    }
-
-    return a;
-}
-
-void mapadd(int* a, int n, int k)
-{
-    for (int i = 0; i < n; i++) {
-        a[i] += k;
-    }
-}
-
-void output(int* a, int n)
-{
-    for (int i = 0; i < n; i++) {
-        printf("%d\n", a[i]);
-    }
 }
