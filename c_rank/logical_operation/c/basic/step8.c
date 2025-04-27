fcf3f54d2567e7bb12ffe32313be741ebe8ab052
kos-y <kosyos.address@gmail.com>
2025-04-27 16:21:13 +0900

    Paiza 問題集 論理演算メニュー 半加算器 C編（paizaランク D 相当）

--- c_rank/logical_operation/c/basic/step8.c
+++ c_rank/logical_operation/c/basic/step8.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 論理演算メニュー 半加算器 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/logical_operation/logical_operation__basic_step8
 #include <stdio.h>
 
 void add_half(int a, int b, int* c, int* s);
