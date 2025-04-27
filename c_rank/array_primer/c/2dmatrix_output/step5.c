b7939ea73b3d74721d016a51e886d1797511fb0f
kos-y <kosyos.address@gmail.com>
2025-04-27 17:37:05 +0900

    Paiza 問題集 配列メニュー 各行の要素数の出力 C編（paizaランク D 相当）

--- c_rank/array_primer/c/2dmatrix_output/step5.c
+++ c_rank/array_primer/c/2dmatrix_output/step5.c
@@ -1,6 +1,7 @@
+// Paiza 問題集 配列メニュー 各行の要素数の出力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_primer/array_primer__2dmatrix_output_step5
 #include <stdio.h>
-
-#define NELEMS(x) sizeof(x) / sizeof(x[0])
+#define NUM_ELE(x) sizeof(x) / sizeof(x[0])
 
 struct V {
     int* v;
@@ -9,17 +10,15 @@ struct V {
 
 int main()
 {
-    int v1[] = {1};
-    int v2[] = {2, 3};
-    int v3[] = {4, 5, 6};
+    int v1[] = {1}, v2[] = {2, 3}, v3[] = {4, 5, 6};
     struct V v[] = {
-        { v1, NELEMS(v1) },
-        { v2, NELEMS(v2) },
-        { v3, NELEMS(v3) }
+        { v1, NUM_ELE(v1) },
+        { v2, NUM_ELE(v2) },
+        { v3, NUM_ELE(v3) }
     };
 
-    for (int i = 0; i < NELEMS(v); i++) {
-        printf("%d\n", v[i].s);
+    for (int i = 0; i < NUM_ELE(v); i++) {
+        printf("%lu\n", v[i].s);
     }
 
     return 0;
