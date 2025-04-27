2a59e9cad705132c7acfc39f52e45850b423fc88
kos-y <kosyos.address@gmail.com>
2025-04-27 16:11:01 +0900

    Paiza 問題集 算術・代入演算メニュー 代入演算 2 C編（paizaランク D 相当）

--- c_rank/arithmetic_substitution_op/c/substitution/step2.c
+++ c_rank/arithmetic_substitution_op/c/substitution/step2.c
@@ -1,11 +1,12 @@
+// Paiza 問題集 算術・代入演算メニュー 代入演算 2 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/arithmetic_substitution_op/arithmetic_substitution_op__substitution_step2
 #include <stdio.h>
 
 int main()
 {
-    int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
+    int a, b, c, n = 0;
 
-    int n = 0;
+    scanf("%d %d %d", &a, &b, &c);
     n += a;
     n *= b;
     n %= c;
