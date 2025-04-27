c10722b3c9f5024638587f2b2588586238ec77e5
kos-y <kosyos.address@gmail.com>
2025-04-27 16:12:53 +0900

    Paiza 問題集 算術・代入演算メニュー 代入演算 4 C編（paizaランク D 相当）

--- c_rank/arithmetic_substitution_op/c/substitution/step4.c
+++ c_rank/arithmetic_substitution_op/c/substitution/step4.c
@@ -1,11 +1,12 @@
+// Paiza 問題集 算術・代入演算メニュー 代入演算 4 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/arithmetic_substitution_op/arithmetic_substitution_op__substitution_step4
 #include <stdio.h>
 
 int main()
 {
-    int a, b;
-    scanf("%d %d", &a, &b);
+    int a, b, n = 10000;
 
-    int n = 10000;
+    scanf("%d %d", &a, &b);
     n /= a;
     n %= b;
     printf("%d\n", n);
