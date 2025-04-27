9f1ee093bd30a8e606b3a1d69dc8d1e0a4f2ea60
kos-y <kosyos.address@gmail.com>
2025-04-27 15:16:30 +0900

    Paiza 問題集 データセット選択メニュー ランダムアクセス C編（paizaランク D 相当）

--- c_rank/data_structure/c/array/step1.c
+++ c_rank/data_structure/c/array/step1.c
@@ -1,13 +1,15 @@
+// Paiza 問題集 データセット選択メニュー ランダムアクセス C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/data_structure/data_structure__array_step1
 #include <stdio.h>
 
 int main()
 {
-    int n, m;
-    scanf("%d %d", &n, &m);
+    int n, m, a;
 
-    int a;
+    scanf("%d %d", &n, &m);
     for (int i = 0; i < n; i++) {
         int t;
+
         scanf("%d", &t);
         if (i == m - 1) {
             a = t;
