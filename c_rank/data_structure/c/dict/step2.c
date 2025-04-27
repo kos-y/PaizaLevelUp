0c4d0693469e1b169b772bab382a117a5725182b
kos-y <kosyos.address@gmail.com>
2025-04-27 15:36:04 +0900

    Paiza 問題集 データセット選択メニュー 英小文字の出現率 C編（paizaランク C 相当）

--- c_rank/data_structure/c/dict/step2.c
+++ c_rank/data_structure/c/dict/step2.c
@@ -1,27 +1,30 @@
+// Paiza 問題集 データセット選択メニュー 英小文字の出現率 C編（paizaランク C 相当）
+// https://paiza.jp/works/mondai/data_structure/data_structure__dict_step2
 #include <stdio.h>
 #include <string.h>
+
+#define CN 26
+#define LEN 100
 
 int main()
 {
     int n;
-    char s[101];
+    char s[LEN + 1], c[CN];
     
     scanf("%d ", &n);
     scanf("%s", s);
-    
-    int c[26];
     memset(c, 0, sizeof(c));
     for (int i = 0; s[i] != '\0'; i++) {
         c[s[i] - 'a']++;
     }
     
-    for (int i = 0; i < 26; i++) {
+    for (int i = 0; i < CN; i++) {
         printf("%d", c[i]);
-        if (i < 25) {
+        if (i < CN - 1) {
             printf(" ");
         }
     }
+
     printf("\n");
-    
     return 0;
 }
