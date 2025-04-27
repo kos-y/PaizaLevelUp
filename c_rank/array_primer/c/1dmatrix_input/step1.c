39b075267278635c1a799ed2ad81ad3942762ffe
kos-y <kosyos.address@gmail.com>
2025-04-27 17:18:56 +0900

    Paiza 問題集 配列メニュー 配列の入力 1 C編（paizaランク D 相当）

--- c_rank/array_primer/c/1dmatrix_input/step1.c
+++ c_rank/array_primer/c/1dmatrix_input/step1.c
@@ -1,7 +1,6 @@
 // Paiza 問題集 配列メニュー 配列の入力 1 C編（paizaランク D 相当）
 // https://paiza.jp/works/mondai/array_primer/array_primer__1dmatrix_input_step1
 #include <stdio.h>
-#include <string.h>
 #define NUM_ELE(x) sizeof(x) / sizeof(x[0])
 
 int main()
