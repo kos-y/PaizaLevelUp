aa0b5b573fc4531f02b442e597ba33fe08cc349c
kos-y <kosyos.address@gmail.com>
2025-04-27 17:20:20 +0900

    Paiza 問題集 配列メニュー  配列の入力 2 C編（paizaランク D 相当）

--- c_rank/array_primer/c/1dmatrix_input/step2.c
+++ c_rank/array_primer/c/1dmatrix_input/step2.c
@@ -1,17 +1,16 @@
+// Paiza 問題集 配列メニュー  配列の入力 2 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_primer/array_primer__1dmatrix_input_step2
 #include <stdio.h>
-
 #define N 10
 
 int main()
 {
-    int a[N];
-
     for (int i = 0; i < N; i++) {
-        scanf("%d", &a[i]);
+        int a;
+
+        scanf("%d", &a);
+        printf("%d\n", a);
     }
 
-    for (int i = 0; i < N; i++) {
-        printf("%d\n", a[i]);
-    }
     return 0;
 }
