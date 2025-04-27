ba41696c5dc3c3379abc08eea72b3acd19bad55a
kos-y <kosyos.address@gmail.com>
2025-04-27 15:32:32 +0900

    Paiza 問題集 データセット選択メニュー 数値の出現率 C編（paizaランク C 相当）

--- c_rank/data_structure/c/dict/step1.c
+++ c_rank/data_structure/c/dict/step1.c
@@ -1,36 +1,32 @@
+// Paiza 問題集 データセット選択メニュー 数値の出現率 C編（paizaランク C 相当）
+// https://paiza.jp/works/mondai/data_structure/data_structure__dict_step1
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define CN 10
+
 int main()
 {
     int n;
-    scanf("%d ", &n);
+    char c[CN];
 
-    char ia[200];
-    fgets(ia, sizeof(ia), stdin);
-    
-    int* pa = (int*)malloc(sizeof(int) * n);
-    char* p = strtok(ia, " ");
-    for (int i = 0; p != NULL; i++) {
-        pa[i] = atoi(p);
-        p = strtok(NULL, " ");
-    }
-    
-    int c[10];
+    scanf("%d", &n);
     memset(c, 0, sizeof(c));
     for (int i = 0; i < n; i++) {
-        c[pa[i]]++;
+        int a;
+
+        scanf("%d", &a);
+        c[a]++;
     }
-    
-    for (int i = 0; i < 10; i++) {
+
+    for (int i = 0; i < CN; i++) {
         printf("%d", c[i]);
-        if (i < 9) {
+        if (i < CN - 1) {
             printf(" ");
         }
     }
+
     printf("\n");
-    
-    free(pa);
     return 0;
 }
