c477f9e9bb1e7076278c4133d3fe003811467735
kos-y <kosyos.address@gmail.com>
2025-04-27 17:28:49 +0900

    Paiza 問題集 配列メニュー 【次元配列の入出力】i番目の出力 Boss C編（paizaランク D 相当）

--- c_rank/array_primer/c/1dmatrix_i-thoutput/boss.c
+++ c_rank/array_primer/c/1dmatrix_i-thoutput/boss.c
@@ -1,16 +1,20 @@
+// Paiza 問題集 配列メニュー 【次元配列の入出力】i番目の出力 Boss C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_primer/array_primer__1dmatrix_i-thoutput_boss
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
     int n, k;
 
     scanf("%d %d", &n, &k);
-    int* pa = (int*)malloc(sizeof(int) * n);
     for (int i = 0; i < n; i++) {
-        scanf("%d", &pa[i]);
+        int a;
+
+        scanf("%d", &a);
+        if (i == k - 1) {
+            printf("%d\n", a);
+        }
     }
-    printf("%d", pa[k - 1]);
-    free(pa);
+
     return 0;
 }
