4370113932f02dc8cce2488e3fdfd0aff3528efa
kos-y <kosyos.address@gmail.com>
2025-04-27 17:41:19 +0900

    Paiza 問題集 配列メニュー 二次元配列の入力 2 C編（paizaランク D 相当）

--- c_rank/array_primer/c/2dmatrix_input/step2.c
+++ c_rank/array_primer/c/2dmatrix_input/step2.c
@@ -1,33 +1,27 @@
+// Paiza 問題集 配列メニュー 二次元配列の入力 2 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_primer/array_primer__2dmatrix_input_step2
 #include <stdio.h>
-#include <stdlib.h>
 
 #define N 5
 
 int main()
 {
     int m;
-    int* pv[N];
 
     scanf("%d", &m);
     for (int i = 0; i < N; i++) {
-        pv[i] = (int*)malloc(sizeof(int) * m);
         for (int j = 0; j < m; j++) {
-            scanf("%d", &pv[i][j]);
-        }
-    }
-    
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < m; j++) {
-            printf("%d", pv[i][j]);
+            int v;
+
+            scanf("%d", &v);
+            printf("%d", v);
             if (j < m - 1) {
                 printf(" ");
             }
         }
+
         printf("\n");
     }
 
-    for (int i = 0; i < N; i++) {
-        free(pv[i]);
-    }
     return 0;
 }
