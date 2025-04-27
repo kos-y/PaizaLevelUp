960cd4fb636f8e4d9aa351415d8a9962fa3ff618
kos-y <kosyos.address@gmail.com>
2025-04-27 16:28:47 +0900

    Paiza 問題集 文字列処理メニュー 文字列の条件判定 C編（paizaランク D 相当）

--- c_rank/string_primer/c/basic/step4.c
+++ c_rank/string_primer/c/basic/step4.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 文字列処理メニュー 文字列の条件判定 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/string_primer/basic_step4
 #include <stdio.h>
 #include <string.h>
 
@@ -7,6 +9,6 @@ int main()
 
     fgets(s, sizeof(s), stdin);
     s[strcspn(s, "\n")] = '\0';
-    printf(strcmp(s, "paiza") == 0 ? "YES" : "NO");
+    printf("%s\n", strcmp(s, "paiza") == 0 ? "YES" : "NO");
     return 0;
 }
