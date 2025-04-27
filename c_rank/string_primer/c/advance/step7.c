56e0677b7588a1d1d4f74fa3c0152bad8ac16aeb
kos-y <kosyos.address@gmail.com>
2025-04-27 16:57:24 +0900

    Paiza 問題集 文字列処理メニュー 表記の訂正 C編（paizaランク C 相当）

--- c_rank/string_primer/c/advance/step7.c
+++ c_rank/string_primer/c/advance/step7.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 文字列処理メニュー 表記の訂正 C編（paizaランク C 相当）
+// https://paiza.jp/works/mondai/string_primer/advance_step7
 #include <stdio.h>
 #include <string.h>
 
@@ -6,9 +8,9 @@ int count_period(char* s);
 int main()
 {
     char s[10001];
+    int i;
     
     scanf("%s", s);
-    int i;
     for (i = 0; s[i] == '0'; i++);
     if (s[i] == '.') {
         i--;
@@ -24,6 +26,7 @@ int main()
 
         while (count_period(s) >= 2) {
             for (i = strlen(s) - 1; s[i] != '.'; i--);
+
             int j;
             for (j = i + 1; s[j] != '.' && s[j] != '\0'; j++) {
                 s[j - 1] = s[j];
