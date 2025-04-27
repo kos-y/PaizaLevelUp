3bca474430720d0cef0bf592a07fc55a0c68e2e6
kos-y <kosyos.address@gmail.com>
2025-04-27 18:43:29 +0900

    Paiza 問題集 配列メニュー 要素の削除 C編（paizaランク D 相当）

--- c_rank/array_primer/c/elm_erase/elm_erase.c
+++ c_rank/array_primer/c/elm_erase/elm_erase.c
@@ -1,71 +1,19 @@
+// Paiza 問題集 配列メニュー 要素の削除 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_primer/array_primer__elm_erase
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-struct V {
-    int i, v;
-    struct V* prev;
-    struct V* next;
-};
 
 int main()
 {
     int n, m;
-    scanf("%d %d ", &n, &m);
-    
-    char ca[30];
-    fgets(ca, sizeof(ca), stdin);
-    
-    char* p = strtok(ca, " ");
-    struct V* p_start = NULL;
-    struct V* p_current = NULL;
-    for (int i = 0; p != NULL; i++, p = strtok(NULL, " ")) {
-        if (p_start == NULL) {
-            p_current = p_start = (struct V*)calloc(1, sizeof(struct V));
-            p_start->i = i;
-            p_start->v = atoi(p);
-        }
-        else {
-            p_current->next = (struct V*)calloc(1, sizeof(struct V));
-            p_current->i = i;
-            p_current->next->v = atoi(p);
-            p_current->next->prev = p_current;
-            p_current = p_current->next;
-        }
-    }
-    
-    p_current = p_start;
-    for (int i = 0; p_current != NULL; i++, p_current = p_current->next) {
-        if (i == m - 1) {
-            struct V* p_target = p_current;
-            if (p_target->prev == NULL) {
-                p_target->next->prev = NULL;
-                p_start = p_target->next;
-                break;
-            }
-            else if (p_target->next == NULL) {
-                p_target->prev->next = NULL;
-            }
-            else {
-                p_target->prev->next = p_target->next;
-                p_target->next->prev = p_target->prev;
-                free(p_target);
-                break;
-            }
-        }
-    }
 
-    p_current = p_start;
-    while (p_current != NULL) {
-        printf("%d\n", p_current->v);
-        p_current = p_current->next;
-    }
-    
-    p_current = p_start;
-    while (p_current != NULL) {
-        struct V* p_buf = p_current->next;
-        free(p_current);
-        p_current = p_buf;
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < n; i++) {
+        int a;
+
+        scanf("%d", &a);
+        if (i != m - 1) {
+            printf("%d\n", a);
+        }
     }
 
     return 0;
