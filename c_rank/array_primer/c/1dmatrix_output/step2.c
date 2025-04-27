58df91f2101423aa06f64de6a311bd807ff3ee09
kos-y <kosyos.address@gmail.com>
2025-04-27 17:14:50 +0900

    Paiza 問題集 配列メニュー 全要素の出力 C編（paizaランク D 相当）

--- c_rank/array_primer/c/1dmatrix_output/step2.c
+++ c_rank/array_primer/c/1dmatrix_output/step2.c
@@ -1,13 +1,16 @@
+// Paiza 問題集 配列メニュー 全要素の出力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_primer/array_primer__1dmatrix_output_step2
 #include <stdio.h>
 #include <string.h>
+#define NUM_ELE(x) sizeof(x) / sizeof(x[0])
 
 int main()
 {
-    char s[] = "5 1 3 4 5 12 6 8 1 3";
-    char* p = strtok(s, " ");
-    while (p != NULL) {
-        printf("%s\n", p);
-        p = strtok(NULL, " ");
+    int n[] = {5, 1, 3, 4, 5, 12, 6, 8, 1, 3};
+
+    for (int i = 0; i < NUM_ELE(n); i++) {
+        printf("%d\n", n[i]);
     }
+
     return 0;
 }
