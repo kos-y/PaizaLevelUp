f5498f2d8a7552fbbb33449fcc38c156ecd061f3
kos-y <kosyos.address@gmail.com>
2025-04-27 17:47:25 +0900

    Paiza 問題集 配列メニュー i番目の出力 2 C編（paizaランク D 相当）

--- c_rank/array_primer/c/2dmatrix_i-thoutput/step2.c
+++ c_rank/array_primer/c/2dmatrix_i-thoutput/step2.c
@@ -1,11 +1,12 @@
+// Paiza 問題集 配列メニュー i番目の出力 2 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_primer/array_primer__2dmatrix_i-thoutput_step2
 #include <stdio.h>
 #define H 3
 #define W 3
 
 int main()
 {
-    int a[H][W];
-    int k, l;
+    int a[H][W], k, l;
 
     scanf("%d %d", &k, &l);
     for (int i = 0; i < H; i++) {
