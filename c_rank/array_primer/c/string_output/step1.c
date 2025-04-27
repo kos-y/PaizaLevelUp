687ea49ae9247a4cc2672c0e1280d5d98724f82d
kos-y <kosyos.address@gmail.com>
2025-04-27 18:47:48 +0900

    Paiza 問題集 配列メニュー 要素数の出力 C編（paizaランク D 相当）

--- c_rank/array_primer/c/string_output/step1.c
+++ c_rank/array_primer/c/string_output/step1.c
@@ -1,10 +1,12 @@
+// Paiza 問題集 配列メニュー 要素数の出力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_primer/array_primer__string_output_step1
 #include <stdio.h>
-#define NELEMS(x) sizeof(x) / sizeof(x[0])
+#define NUM_ELE(x) sizeof(x) / sizeof(x[0])
 
 int main()
 {
     char* sz[] = { "Hello", "paiza", "1234", "pa13" };
     
-    printf("%lu\n", NELEMS(sz));
+    printf("%lu\n", NUM_ELE(sz));
     return 0;
 }
