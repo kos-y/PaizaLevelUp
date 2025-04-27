8da8f67e895d312dbded7549ea6de62818a8d1c2
kos-y <kosyos.address@gmail.com>
2025-04-27 16:44:54 +0900

    Paiza 問題集 文字列処理メニュー 文字列の検索 C編（paizaランク D 相当）

--- c_rank/string_primer/c/normal/step9.c
+++ c_rank/string_primer/c/normal/step9.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 文字列処理メニュー 文字列の検索 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/string_primer/normal_step9
 #include <stdio.h>
 #include <string.h>
 
@@ -11,6 +13,6 @@ int main()
     fgets(t, sizeof(t), stdin);
     s[strcspn(s, "\n")] = '\0';
     t[strcspn(t, "\n")] = '\0';
-    printf(strstr(s, t) != NULL ? "YES" : "NO");
+    printf("%s\n", strstr(s, t) != NULL ? "YES" : "NO");
     return 0;
 }
