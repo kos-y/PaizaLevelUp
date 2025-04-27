13553d7d42b37c563c8cd555851d32827df6f2b0
kos-y <kosyos.address@gmail.com>
2025-04-27 19:01:31 +0900

    Paiza 問題集 配列メニュー 辞書順に出力 3 C編（paizaランク D 相当）

--- c_rank/array_primer/c/string_dictionary/step3.c
+++ c_rank/array_primer/c/string_dictionary/step3.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 配列メニュー 辞書順に出力 3 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_primer/array_primer__string_dictionary_step3
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
