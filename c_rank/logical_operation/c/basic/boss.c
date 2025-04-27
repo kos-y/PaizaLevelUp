411d77974063a72ac1ed9a9144e818b8388fcee3
kos-y <kosyos.address@gmail.com>
2025-04-27 16:23:49 +0900

    Paiza 問題集 論理演算メニュー 論理演算を用いた計算のまとめ C編（paizaランク C 相当）

--- c_rank/logical_operation/c/basic/boss.c
+++ c_rank/logical_operation/c/basic/boss.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 論理演算メニュー 論理演算を用いた計算のまとめ C編（paizaランク C 相当）
+// https://paiza.jp/works/mondai/logical_operation/logical_operation__basic_boss
 #include <stdio.h>
 
 int not(int x);
@@ -8,11 +10,10 @@ int main()
     
     scanf("%d %d %d %d", &a, &b, &c, &d);
     printf("%d\n", not((not(a) & not(b)) | not(c)) ^ d);
-    
     return 0;
 }
 
 int not(int x)
 {
-    return (~x) & 0x0001;
+    return (~x) & 0x1;
 }
