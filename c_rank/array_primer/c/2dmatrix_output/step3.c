53f251ec552bd0d7c7742957d0eb9c011f195f27
kos-y <kosyos.address@gmail.com>
2025-04-27 17:33:40 +0900

    Paiza 問題集 配列メニュー 行数の出力 C編（paizaランク D 相当）

--- c_rank/array_primer/c/2dmatrix_output/step3.c
+++ c_rank/array_primer/c/2dmatrix_output/step3.c
@@ -1,7 +1,9 @@
+// Paiza 問題集 配列メニュー 行数の出力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_primer/array_primer__2dmatrix_output_step3
 #include <stdio.h>
-
 #define H 3
 #define W 3
+#define NUM_ELE(x) sizeof(x) / sizeof(x[0])
 
 int main()
 {
@@ -11,6 +13,6 @@ int main()
         {7, 7, 9}
     };
 
-    printf("%lu\n", sizeof(n) / sizeof(n[0]));
+    printf("%lu\n", NUM_ELE(n));
     return 0;
 }
