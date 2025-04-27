d6936e0b17e6ce148f9b0d3e54718e0a93b5338c
kos-y <kosyos.address@gmail.com>
2025-04-27 16:16:20 +0900

    Paiza 問題集 論理演算メニュー 否定( NOT )の基本 C編（paizaランク D 相当）

--- c_rank/logical_operation/c/basic/step3.c
+++ c_rank/logical_operation/c/basic/step3.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 論理演算メニュー 否定( NOT )の基本 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/logical_operation/logical_operation__basic_step3
 #include <stdio.h>
 
 int main()
@@ -5,6 +7,6 @@ int main()
     int a;
 
     scanf("%d", &a);
-    printf("%d\n", (~a) & 0x0001);
+    printf("%d\n", (~a) & 0x1);
     return 0;
 }
