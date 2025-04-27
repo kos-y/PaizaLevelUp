d4044360db050698d8060cdbde15ec0e28b51d2a
kos-y <kosyos.address@gmail.com>
2025-04-27 16:11:43 +0900

    Paiza 問題集 算術・代入演算メニュー 代入演算 3 C編（paizaランク D 相当）

--- c_rank/arithmetic_substitution_op/c/substitution/step3.c
+++ c_rank/arithmetic_substitution_op/c/substitution/step3.c
@@ -1,8 +1,11 @@
+// Paiza 問題集 算術・代入演算メニュー 代入演算 3 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/arithmetic_substitution_op/arithmetic_substitution_op__substitution_step3
 #include <stdio.h>
 
 int main()
 {
     int n = 10000;
+    
     n /= 361;
     n %= 28;
     printf("%d\n", n);
