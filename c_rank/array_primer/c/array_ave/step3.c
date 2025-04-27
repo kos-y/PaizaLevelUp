9427f8bf97c5eded7c83be36027b10a0df5a1e24
kos-y <kosyos.address@gmail.com>
2025-04-27 19:43:23 +0900

    Paiza 問題集 配列メニュー ある数以上以下の要素の列挙 3 C編（paizaランク D 相当）

--- c_rank/array_primer/c/array_ave/step3.c
+++ c_rank/array_primer/c/array_ave/step3.c
@@ -1,31 +1,26 @@
+// Paiza 問題集 配列メニュー ある数以上以下の要素の列挙 3 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_primer/array_primer__array_ave_step3
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int main()
 {
-    int n;
-    char ca[1011];
+    int n, sum = 0;
 
-    scanf("%d ", &n);    
-    fgets(ca, sizeof(ca), stdin);
-    ca[strcspn(ca, "\n")] = '\0';
-    int* a = (int*)calloc(n, sizeof(int));
-    char* p = strtok(ca, " ");
-    int sum = 0;
-    for (int i = 0; p != NULL; i++) {
-        a[i] = atoi(p);
-        sum += a[i];
-        p = strtok(NULL, " ");
+    scanf("%d", &n);
+    int* pa = (int*)calloc(n, sizeof(int));
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &pa[i]);
+        sum += pa[i];
     }
-    
+
     double ave = (double)sum / (double)n;
     for (int i = 0; i < n; i++) {
-        if ((double)a[i] >= ave) {
-            printf("%d\n", a[i]);
+        if ((double)pa[i] >= ave) {
+            printf("%d\n", pa[i]);
         }
     }
 
-    free(a);
+    free(pa);
     return 0;
 }
