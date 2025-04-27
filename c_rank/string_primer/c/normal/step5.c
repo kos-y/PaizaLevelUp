5c9bb936e74f2a6c2e894d12004196559df39e70
kos-y <kosyos.address@gmail.com>
2025-04-27 16:39:55 +0900

    Paiza 問題集 文字列処理メニュー 数値から文字列への変換 C編（paizaランク D 相当）

--- c_rank/string_primer/c/normal/step5.c
+++ c_rank/string_primer/c/normal/step5.c
@@ -1,15 +1,16 @@
+// Paiza 問題集 文字列処理メニュー 数値から文字列への変換 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/string_primer/normal_step5
 #include <stdio.h>
 
 int main()
 {
     long long x, y;
     int n;
+    char s[14];
     
     scanf("%lld", &x);
     scanf("%lld", &y);
     scanf("%d", &n);
-    
-    char s[14];
     snprintf(s, sizeof(s), "%lld", x + y);
     printf("%c\n", s[n - 1]);
     return 0;
