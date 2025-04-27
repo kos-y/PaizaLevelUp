f8793c7e17ec07313d152bde222e567ad980a574
kos-y <kosyos.address@gmail.com>
2025-04-27 16:06:26 +0900

    Paiza 問題集 算術・代入演算メニュー 累乗 C編（paizaランク D 相当）

--- c_rank/arithmetic_substitution_op/c/arithmetic/step5.c
+++ c_rank/arithmetic_substitution_op/c/arithmetic/step5.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 算術・代入演算メニュー 累乗 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/arithmetic_substitution_op/arithmetic_substitution_op__arithmetic_step5
 #include <stdio.h>
 #include <math.h>
 
