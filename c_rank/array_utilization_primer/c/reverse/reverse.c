46219b69a7c052be6c06979ea032a7b1431bf002
kos-y <kosyos.address@gmail.com>
2025-04-28 19:24:03 +0900

    Paiza 問題集 配列活用メニュー 配列の順序の反転 C編（paizaランク D 相当）

--- c_rank/array_utilization_primer/c/reverse/reverse.c
+++ c_rank/array_utilization_primer/c/reverse/reverse.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 配列活用メニュー 配列の順序の反転 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__reverse
 #include <stdio.h>
 #include <stdlib.h>
 
