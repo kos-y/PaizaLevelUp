04dd59fedb63c606faad3bb15c326c64712ed58b
kos-y <kosyos.address@gmail.com>
2025-04-28 19:25:36 +0900

    Paiza 問題集 配列活用メニュー 変数の入れ替え C編（paizaランク D 相当）

--- c_rank/array_utilization_primer/c/excange/excange.c
+++ c_rank/array_utilization_primer/c/excange/excange.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 配列活用メニュー 変数の入れ替え C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__excange
 #include <stdio.h>
 
 void swap(int* pa, int* pb);
