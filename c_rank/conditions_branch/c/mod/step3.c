cd3017aee7d635855685c66c70c1172726637d8f
kos-y <kosyos.address@gmail.com>
2025-04-28 20:03:33 +0900

    Paiza 問題集 条件分岐メニュー 偶奇の判定 C編（paizaランク D 相当）

--- c_rank/conditions_branch/c/mod/step3.c
+++ c_rank/conditions_branch/c/mod/step3.c
@@ -1,21 +1,18 @@
+// Paiza 問題集 条件分岐メニュー 偶奇の判定 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/conditions_branch/conditions_branch__mod_step3
 #include <stdio.h>
 
 int main()
 {
-    int n, e = 0, o = 0;
+    int n, c[2] = {0, 0};
 
     scanf("%d", &n);
     for (int i = 0; i < n; i++) {
         int a;
 
         scanf("%d", &a);
-        if (a % 2 == 0) {
-            e++;
-        }
-        else {
-            o++;
-        }
+        c[a % 2]++;
     }
-    printf("%d %d\n", e, o);
+    printf("%d %d\n", c[0], c[1]);
     return 0;
 }
