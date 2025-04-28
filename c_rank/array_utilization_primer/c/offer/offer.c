3414f25e056b89438caedd8be6f0e489600c1246
kos-y <kosyos.address@gmail.com>
2025-04-28 19:41:05 +0900

    Paiza 問題集 配列活用メニュー 内定 C編（paizaランク C 相当）

--- c_rank/array_utilization_primer/c/offer/offer.c
+++ c_rank/array_utilization_primer/c/offer/offer.c
@@ -1,9 +1,12 @@
+// Paiza 問題集 配列活用メニュー 内定 C編（paizaランク C 相当）
+// https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__offer
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
     int n, k, m;
+    
     scanf("%d %d %d", &n, &k, &m);
     
     int* a = (int*)calloc(n, sizeof(int));
