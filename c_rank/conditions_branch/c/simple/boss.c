58d07eb21add47eb784e6dbbc2b2ada2c9d6b2b0
kos-y <kosyos.address@gmail.com>
2025-04-28 19:53:26 +0900

    Paiza 問題集 条件分岐メニュー 0 が含まれていないか判定 C編（paizaランク D 相当）

--- c_rank/conditions_branch/c/simple/boss.c
+++ c_rank/conditions_branch/c/simple/boss.c
@@ -1,29 +1,23 @@
+// Paiza 問題集 条件分岐メニュー 0 が含まれていないか判定 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/conditions_branch/conditions_branch__simple_boss
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
-
-bool find0(int* pa, int n);
 
 int main()
 {
     int n;
+    bool f = false;
 
     scanf("%d", &n);
-    int* pa = (int*)calloc(n, sizeof(int));
     for (int i = 0; i < n; i++) {
-        scanf("%d", &pa[i]);
-    }
-    printf("%s\n", !find0(pa, n) ? "YES" : "NO");
-    return 0;
-}
+        int a;
 
-bool find0(int* pa, int n)
-{
-    for (int i = 0; i < n; i++) {
-        if (pa[i] == 0) {
-            return true;
+        scanf("%d", &a);
+        if (a == 0) {
+            f = true;
         }
     }
 
-    return false;
+    printf("%s\n", !f ? "YES" : "NO");
+    return 0;
 }
