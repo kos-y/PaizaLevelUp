5789087f1f2243799bd50a8eb8f9eb56f4f4871d
kos-y <kosyos.address@gmail.com>
2025-04-28 20:27:13 +0900

    Paiza 問題集 ループメニュー1 数列の和 C編（paizaランク D 相当）

--- c_rank/loop_problems/c/seq_sum/seq_sum.c
+++ c_rank/loop_problems/c/seq_sum/seq_sum.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 ループメニュー1 数列の和 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/loop_problems/loop_problems__seq_sum
 #include <stdio.h>
 
 int main()
@@ -7,8 +9,8 @@ int main()
     scanf("%d", &n);
     for (int i = 0; i < n; i++) {
         int a;
+        
         scanf("%d", &a);
-
         sum += a;
     }
 
