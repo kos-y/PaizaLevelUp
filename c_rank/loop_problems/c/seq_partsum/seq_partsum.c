3a26e2c6d19b96aa5becce91073e4ef44eaf0e02
kos-y <kosyos.address@gmail.com>
2025-04-28 20:29:52 +0900

    Paiza 問題集 ループメニュー1 数列の A 番目から B 番目までの和 C編（paizaランク D 相当）

--- c_rank/loop_problems/c/seq_partsum/seq_partsum.c
+++ c_rank/loop_problems/c/seq_partsum/seq_partsum.c
@@ -1,21 +1,21 @@
+// Paiza 問題集 ループメニュー1 数列の A 番目から B 番目までの和 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/loop_problems/loop_problems__seq_partsum
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
     int n, a, b, sum = 0;
 
     scanf("%d %d %d", &n, &a, &b);
-    int* pa = (int*)calloc(n, sizeof(int));
     for (int i = 0; i < n; i++) {
-        scanf("%d", &pa[i]);
-    }
+        int v;
 
-    for (int i = a - 1; i < b; i++) {
-        sum += pa[i];
+        scanf("%d", &v);
+        if (a - 1 <= i && i < b) {
+            sum += v;
+        }
     }
 
     printf("%d\n", sum);
-    free(pa);
     return 0;
 }
