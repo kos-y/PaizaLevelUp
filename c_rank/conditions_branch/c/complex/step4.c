55377ad19507cff50780e86f5c20d7b89f78045e
kos-y <kosyos.address@gmail.com>
2025-04-28 20:12:38 +0900

    Paiza 問題集 終了判定 C編（paizaランク C 相当）

--- c_rank/conditions_branch/c/complex/step4.c
+++ c_rank/conditions_branch/c/complex/step4.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 終了判定 C編（paizaランク C 相当）
+// https://paiza.jp/works/mondai/conditions_branch/conditions_branch__complex_step4
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,13 +13,16 @@ int main()
     for (int i = 0; i < n; i++) {
         scanf("%d", &pa[i]);
     }
+
     int sum = 0;
     for (int i = 0; i < n; i++) {
         if (pa[i] % 2 != 0) {
             break;
         }
+
         sum += pa[i];
     }
+    
     printf("%d\n", sum);
     return 0;
 }
