d0f7e2befedb490201f7782716d22bcc25ed6b21
kos-y <kosyos.address@gmail.com>
2025-04-28 20:21:47 +0900

    Paiza 問題集 ループメニュー1 数字の受け取り 1 C編（paizaランク D 相当）

--- c_rank/loop_problems/c/input_num/step1.c
+++ c_rank/loop_problems/c/input_num/step1.c
@@ -1,16 +1,15 @@
+// Paiza 問題集 ループメニュー1 数字の受け取り 1 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/loop_problems/loop_problems__input_num_step1
 #include <stdio.h>
 #define N 10
 
 int main()
 {
-    int k[N];
-
     for (int i = 0; i < N; i++) {
-        scanf("%d", &k[i]);
-    }
+        int k;
 
-    for (int i = 0; i < N; i++) {
-        printf("%d\n", k[i]);
+        scanf("%d", &k);
+        printf("%d\n", k);
     }
 
     return 0;
