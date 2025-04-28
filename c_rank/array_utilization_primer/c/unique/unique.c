d555a5e5d7eaabcb5d91beab5520caec5c5ccb2e
kos-y <kosyos.address@gmail.com>
2025-04-28 19:34:56 +0900

    Paiza 問題集 配列活用メニュー 重複要素の削除 C編（paizaランク D 相当）

--- c_rank/array_utilization_primer/c/unique/unique.c
+++ c_rank/array_utilization_primer/c/unique/unique.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 配列活用メニュー 重複要素の削除 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__unique
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
