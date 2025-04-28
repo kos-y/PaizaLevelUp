aec7457516bed66494b5cb6b1f7b7ae24ae23c66
kos-y <kosyos.address@gmail.com>
2025-04-28 20:09:25 +0900

    Paiza 問題集 条件分岐メニュー 足したり引いたり C編（paizaランク C 相当）

--- c_rank/conditions_branch/c/complex/step2.c
+++ c_rank/conditions_branch/c/complex/step2.c
@@ -1,25 +1,17 @@
+// Paiza 問題集 条件分岐メニュー 足したり引いたり C編（paizaランク C 相当）
+// https://paiza.jp/works/mondai/conditions_branch/conditions_branch__complex_step2
 #include <stdio.h>
 
 int main()
 {
     int n, a, b;
+
     scanf("%d %d %d", &n, &a, &b);
-    
     for (int i = 0; i < 4; i++) {
         int t = n;
-        if (i < 2) {
-            t += a;
-        }
-        else {
-            t -= a;
-        }
-        if (i % 2 == 0) {
-            t += b;
-        }
-        else {
-            t -= b;
-        }
-        
+
+        t += i < 2 ? a : -a;
+        t += i % 2 ? b : -b;
         if (t == 0) {
             printf("YES\n");
             return 0;
