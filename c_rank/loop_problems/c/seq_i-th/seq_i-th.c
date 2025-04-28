c320c71d5bd188dbf2a4f98116f83b65b6b68c27
kos-y <kosyos.address@gmail.com>
2025-04-28 20:45:00 +0900

    Paiza 問題集 ループメニュー1 数列の何番目にある? C編（paizaランク D 相当）

--- c_rank/loop_problems/c/seq_i-th/seq_i-th.c
+++ c_rank/loop_problems/c/seq_i-th/seq_i-th.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 ループメニュー1 数列の何番目にある? C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/loop_problems/loop_problems__seq_i-th
 #include <stdio.h>
 
 int main()
@@ -5,11 +7,12 @@ int main()
     int n, x = -1;
 
     scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
         int a;
+
         scanf("%d", &a);
         if (x == -1 && a == 1) {
-            x = i + 1;
+            x = i;
         }
     }
 
