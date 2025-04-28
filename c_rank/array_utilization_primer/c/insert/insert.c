0d0b41fe3fe08a1648c5f241202cdd025811d358
kos-y <kosyos.address@gmail.com>
2025-04-28 19:29:10 +0900

    Paiza 問題集 配列活用メニュー 指定位置への要素の追加 C編（paizaランク D 相当）

--- c_rank/array_utilization_primer/c/insert/insert.c
+++ c_rank/array_utilization_primer/c/insert/insert.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 配列活用メニュー 指定位置への要素の追加 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__insert
 #include <stdio.h>
 #include <stdlib.h>
 
