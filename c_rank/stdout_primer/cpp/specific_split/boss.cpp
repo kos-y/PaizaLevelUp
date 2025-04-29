aa90ce1dba3eff9d50564593eaaac09200f4e670
kos-y <kosyos.address@gmail.com>
2025-04-29 19:55:57 +0900

    Paiza 問題集 標準出力メニュー 【特定の文字で区切り 1 行で出力】大きな数値を 3 けたごとにカンマ区切りで出力 2 C++編（paizaランク C 相当）

---
+++ c_rank/stdout_primer/cpp/specific_split/boss.cpp
@@ -0,0 +1,23 @@
+// Paiza 問題集 標準出力メニュー 【特定の文字で区切り 1 行で出力】大きな数値を 3 けたごとにカンマ区切りで出力 2 C++編（paizaランク C 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__specific_split_boss
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string n;
+    cin >> n;
+
+    string r;
+    int c = 0;
+    for (reverse_iterator<string::iterator> it = n.rbegin(); it != n.rend(); ++it) {
+        r += *it;
+        if (++c % 3 == 0 && (it + 1) != n.rend()) {
+            r += ',';
+        }
+    }
+
+    reverse(r.begin(), r.end());
+    cout << r << endl;
+    return 0;
+}
