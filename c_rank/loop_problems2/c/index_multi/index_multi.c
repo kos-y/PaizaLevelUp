d982b79b63a4f9792e89aed14a2a124788aa8496
kos-y <kosyos.address@gmail.com>
2025-04-29 09:20:21 +0900

    Paiza 問題集 ループメニュー1 カウント変数を使った計算 C編（paizaランク D 相当）

--- c_rank/loop_problems2/c/index_multi/index_multi.c
+++ c_rank/loop_problems2/c/index_multi/index_multi.c
@@ -1,20 +1,18 @@
+// Paiza 問題集 ループメニュー1 カウント変数を使った計算 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/loop_problems2/loop_problems2__index_multi
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
     int n;
 
     scanf("%d", &n);
-    int* pm = (int*)calloc(n, sizeof(int));
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &pm[i]);
+    for (int i = 1; i <= n; i++) {
+        int m;
+
+        scanf("%d", &m);
+        printf("%d\n", m * i);
     }
 
-    for (int i = 0; i < n; i++) {
-        printf("%d\n", pm[i] * (i + 1));
-    }
-
-    free(pm);
     return 0;
 }
