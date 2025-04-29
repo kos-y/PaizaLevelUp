2fd0c72a6df0416aa842487a28f4820b041cb9e6
kos-y <kosyos.address@gmail.com>
2025-04-29 19:23:07 +0900

    Paiza 問題集 標準出力メニュー カンマ区切りで 2 つ出力 C++編（paizaランク D 相当）

---
+++ c_rank/stdout_primer/cpp/specific_split/step1.cpp
@@ -0,0 +1,13 @@
+// Paiza 問題集 標準出力メニュー カンマ区切りで 2 つ出力 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__specific_split_step1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m;
+
+    cin >> n >> m;
+    cout << n << "," << m << endl;
+    return 0;
+}
