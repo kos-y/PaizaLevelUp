d9e6daf91b6a0a51577206bec9fd4e9259b2c0c3
kos-y <kosyos.address@gmail.com>
2025-04-29 14:15:00 +0900

    Paiza 問題集 条件式メニュー NOT 演算子 2 C編（paizaランク D 相当）

--- c_rank/conditions_expression/c/logical_operator/step4.c
+++ c_rank/conditions_expression/c/logical_operator/step4.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 条件式メニュー NOT 演算子 2 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/conditions_expression/conditions_expression__logical_operator_step4
 #include <stdio.h>
 
 int main()
