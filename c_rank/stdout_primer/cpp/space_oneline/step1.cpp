d6164867709b30ddbd34cc3548da4c314e121ee4
kos-y <kosyos.address@gmail.com>
2025-04-29 18:22:27 +0900

    Paiza 問題集 標準出力メニュー 2 つの数値を出力 C++編（paizaランク D 相当）

---
+++ c_rank/stdout_primer/cpp/space_oneline/step1.cpp
@@ -0,0 +1,10 @@
+// Paiza 問題集 標準出力メニュー 2 つの数値を出力 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__space_oneline_step1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << 1 << " " << 1 << endl;
+    return 0;
+}
