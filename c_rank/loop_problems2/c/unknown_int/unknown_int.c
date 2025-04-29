d51cf8ebc8cb0b8c78c98b4816024327ca5643ab
kos-y <kosyos.address@gmail.com>
2025-04-29 09:07:47 +0900

    Paiza 問題集 ループメニュー1 未知数個の数の受け取り C編（paizaランク D 相当）

--- c_rank/loop_problems2/c/unknown_int/unknown_int.c
+++ c_rank/loop_problems2/c/unknown_int/unknown_int.c
@@ -1,47 +1,17 @@
+// Paiza 問題集 ループメニュー1 未知数個の数の受け取り C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/loop_problems2/loop_problems2__unknown_int
 #include <stdio.h>
-#include <stdlib.h>
-
-struct V
-{
-    int v;
-    struct V* pn;
-};
 
 int main()
 {
-    int a;
-    struct V* ps = NULL;
-    struct V* pc = NULL;
-
     while (1) {
-        scanf("%d", &a);
-        if (ps == NULL) {
-            ps = (struct V*)calloc(1, sizeof(struct V));
-            ps->v = a;
-            pc = ps;
-        }
-        else {
-            pc->pn = (struct V*)calloc(1, sizeof(struct V));
-            pc->pn->v = a;
-            pc = pc->pn;
-        }
+        int a;
 
+        scanf("%d", &a);
+        printf("%d\n", a);
         if (a == -1) {
             break;
         }
-    }
-
-    pc = ps;
-    while (pc != NULL) {
-        printf("%d\n", pc->v);
-        pc = pc->pn;
-    }
-
-    pc = ps;
-    while (pc != NULL) {
-        struct V* p = pc->pn;
-        free(pc);
-        pc = p;
     }
 
     return 0;
