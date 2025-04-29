f0558d6adacc01e0f78cea2261c7b622a750dfcc
kos-y <kosyos.address@gmail.com>
2025-04-29 09:35:26 +0900

    Paiza 問題集 二重ループメニュー 指定された数字までの出力 C編（paizaランク D 相当）

--- c_rank/double_roop_problems/c/print_column_new/print_column_new.c
+++ c_rank/double_roop_problems/c/print_column_new/print_column_new.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 二重ループメニュー 指定された数字までの出力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/double_roop_problems/double_roop_problems__print_column_new
 #include <stdio.h>
 
 int main()
@@ -11,6 +13,7 @@ int main()
             printf(" ");
         }
     }
+    
     printf("\n");
     return 0;
 }
