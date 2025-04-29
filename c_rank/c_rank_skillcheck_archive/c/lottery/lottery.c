a2902c9f4b72a8a557e51582faf0f2bd14d31a33
kos-y <kosyos.address@gmail.com>
2025-04-29 14:57:43 +0900

    Paiza 問題集 Cランク・スキルチェック過去問題セット 宝くじ C編（paizaランク C 相当）

--- c_rank/c_rank_skillcheck_archive/c/lottery/lottery.c
+++ c_rank/c_rank_skillcheck_archive/c/lottery/lottery.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 Cランク・スキルチェック過去問題セット 宝くじ C編（paizaランク C 相当）
+// https://paiza.jp/works/mondai/c_rank_skillcheck_archive/lottery
 #include <stdio.h>
 
 int main()
@@ -11,20 +13,15 @@ int main()
         int a;
 
         scanf("%d", &a);
-
         if (a == b) {
             printf("first\n");
-        }
-        else if (b - 1 <= a && a <= b + 1) {
+        } else if (b - 1 <= a && a <= b + 1) {
             printf("adjacent\n");
-        }
-        else if (a % 10000 == b % 10000) {
+        } else if (a % 10000 == b % 10000) {
             printf("second\n");
-        }
-        else if (a % 1000 == b % 1000) {
+        } else if (a % 1000 == b % 1000) {
             printf("third\n");
-        }
-        else {
+        } else {
             printf("blank\n");
         }
     }
