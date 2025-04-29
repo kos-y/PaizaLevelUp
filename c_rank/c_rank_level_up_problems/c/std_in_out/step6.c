7d2c58019766a6423b5692df6a9b8e04b5f27c26
kos-y <kosyos.address@gmail.com>
2025-04-29 10:14:36 +0900

    Paiza 問題集 Cランクレベルアップメニュー 改行区切りでの出力 C編（paizaランク D 相当）

--- c_rank/c_rank_level_up_problems/c/std_in_out/step6.c
+++ c_rank/c_rank_level_up_problems/c/std_in_out/step6.c
@@ -1,20 +1,18 @@
+// Paiza 問題集 Cランクレベルアップメニュー 改行区切りでの出力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/c_rank_level_up_problems/c_rank_std_in_out_step6
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
     int n;
 
     scanf("%d", &n);
-    int* pa = (int*)calloc(n, sizeof(int));
     for (int i = 0; i < n; i++) {
-        scanf("%d", &pa[i]);
+        int a;
+
+        scanf("%d", &a);
+        printf("%d\n", a);
     }
 
-    for (int i = 0; i < n; i++) {
-        printf("%d\n", pa[i]);
-    }
-
-    free(pa);
     return 0;
 }
