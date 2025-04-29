09575a9aa0efbbbaab872dde0caabbb16cfb98e8
kos-y <kosyos.address@gmail.com>
2025-04-29 18:14:51 +0900

    Paiza 問題集 標準出力メニュー 2 行の出力 C++編（paizaランク D 相当）

---
+++ c_rank/stdout_primer/cpp/newline_digit/step2.cpp
@@ -0,0 +1,11 @@
+// Paiza 問題集 標準出力メニュー 2 行の出力 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__newline_digit_step2
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << 8 << endl;
+    cout << 13 << endl;
+    return 0;
+}
