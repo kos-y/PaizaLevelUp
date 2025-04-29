07410e08bc9edcb5cf2effd3ddde4576e9238e5b
kos-y <kosyos.address@gmail.com>
2025-04-29 10:35:40 +0900

    Paiza 問題集 Cランクレベルアップメニュー インデックス取得 C編（paizaランク D 相当）

--- c_rank/c_rank_level_up_problems/c/for/step3.c
+++ c_rank/c_rank_level_up_problems/c/for/step3.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 Cランクレベルアップメニュー インデックス取得 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/c_rank_level_up_problems/c_rank_for_step3
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,7 +7,7 @@ int main()
 {
     int n, k;
 
-    scanf("%d ", &n);    
+    scanf("%d ", &n);
     int* a = (int*)calloc(n, sizeof(int));
     for (int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
