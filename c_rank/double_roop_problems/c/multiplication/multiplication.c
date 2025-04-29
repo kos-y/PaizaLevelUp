5523969bff5faaf927ac7b37b5b3330b7cdd7d9d
kos-y <kosyos.address@gmail.com>
2025-04-29 09:58:37 +0900

    Paiza 問題集 二重ループメニュー かけ算表 C編（paizaランク D 相当）

--- c_rank/double_roop_problems/c/multiplication/multiplication.c
+++ c_rank/double_roop_problems/c/multiplication/multiplication.c
@@ -1,14 +1,16 @@
+// Paiza 問題集 二重ループメニュー かけ算表 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/double_roop_problems/double_roop_problems__multiplication
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
     int n;
-    
-    scanf("%d ", &n);
+
+    scanf("%d", &n);
     int* pa = (int*)calloc(n, sizeof(int));
     for (int i = 0; i < n; i++) {
-        scanf("%d ", &pa[i]);
+        scanf("%d", &pa[i]);
     }
     
     for (int i = 0; i < n; i++) {
