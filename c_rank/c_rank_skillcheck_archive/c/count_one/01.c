8d985a49e79b2f5935973a233d8bc24da9542fb0
kos-y <kosyos.address@gmail.com>
2025-04-29 15:30:39 +0900

    Paiza 問題集 Cランク・スキルチェック過去問題セット 「1を数えよ」を解くために:part1 C編（paizaランク D 相当）

---
+++ c_rank/c_rank_skillcheck_archive/c/count_one/01.c
@@ -0,0 +1,12 @@
+// Paiza 問題集 Cランク・スキルチェック過去問題セット 「1を数えよ」を解くために:part1 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/c_rank_skillcheck_archive/count_one_01
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    scanf("%d", &n);
+    printf("%d\n", n);
+    return 0;
+}
