2bc5770c971573804f7932f2a0bc9407fc9b3924
kos-y <kosyos.address@gmail.com>
2025-04-29 09:14:29 +0900

    Paiza 問題集 ループメニュー1 割り切れる数だけ出力 C編（paizaランク D 相当）

--- c_rank/loop_problems2/c/div_output/div_output.c
+++ c_rank/loop_problems2/c/div_output/div_output.c
@@ -1,22 +1,20 @@
+// Paiza 問題集 ループメニュー1 割り切れる数だけ出力 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/loop_problems2/loop_problems2__div_output
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
     int n;
 
     scanf("%d", &n);
-    int* pa = (int*)calloc(n, sizeof(int));
     for (int i = 0; i < n; i++) {
-        scanf("%d", &pa[i]);
-    }
+        int a;
 
-    for (int i = 0; i < n; i++) {
-        if (pa[i] % 3 == 0) {
-            printf("%d\n", pa[i]);
+        scanf("%d", &a);
+        if (a % 3 == 0) {
+            printf("%d\n", a);
         }
     }
 
-    free(pa);
     return 0;
 }
