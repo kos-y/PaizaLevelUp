22db25027677315a9648f9a34ee6b2a4e36e53bf
kos-y <kosyos.address@gmail.com>
2025-04-29 19:03:25 +0900

    Paiza 問題集 標準出力メニュー 入力された 2 つの文字列を出力 C++編（paizaランク D 相当）

---
+++ c_rank/stdout_primer/cpp/string_output/step3.cpp
@@ -0,0 +1,16 @@
+// Paiza 問題集 標準出力メニュー 入力された 2 つの文字列を出力 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__string_output_step3
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s, t;
+
+    cin >> s;
+    cin >> t;
+    cout << s << endl;
+    cout << t << endl;
+    return 0;
+}
