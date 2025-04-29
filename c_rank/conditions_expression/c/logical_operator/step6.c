2567cd7640e34229003e52ff200d7ffef013d512
kos-y <kosyos.address@gmail.com>
2025-04-29 14:16:53 +0900

    Paiza 問題集 条件式メニュー 論理演算子の組み合わせ 2 C編（paizaランク D 相当）

--- c_rank/conditions_expression/c/logical_operator/step6.c
+++ c_rank/conditions_expression/c/logical_operator/step6.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 条件式メニュー 論理演算子の組み合わせ 2 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/conditions_expression/conditions_expression__logical_operator_step6
 #include <stdio.h>
 
 int main()
