2a9b844934151c4bff368cd3c4ec02fa63c867ce
kos-y <kosyos.address@gmail.com>
2025-04-29 21:30:08 +0900

    Paiza 問題集 標準出力メニュー 標準出力メニュー 1 つの実数を出力 3 C++編（paizaランク D 相当）

---
+++ c_rank/stdout_primer/cpp/format_real_number/step3.cpp
@@ -0,0 +1,14 @@
+// Paiza 問題集 標準出力メニュー 標準出力メニュー 1 つの実数を出力 3 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__format_real_number_step3
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double n;
+
+    cin >> n;
+    cout << fixed << setprecision(3) << n << endl;
+    return 0;
+}
