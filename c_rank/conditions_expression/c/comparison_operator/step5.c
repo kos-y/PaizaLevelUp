6339585c7ec6e6123f4eeb5e497349d68509ff70
kos-y <kosyos.address@gmail.com>
2025-04-29 11:01:23 +0900

    Paiza 問題集 条件式メニュー 式の比較 C編（paizaランク D 相当）

--- c_rank/conditions_expression/c/comparison_operator/step5.c
+++ c_rank/conditions_expression/c/comparison_operator/step5.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 条件式メニュー 式の比較 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/conditions_expression/conditions_expression__comparison_operator_step5
 #include <stdio.h>
 
 int main()
