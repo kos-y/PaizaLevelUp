d9584b69b7d73165d3b66a04b764a3f4e6330ce7
kos-y <kosyos.address@gmail.com>
2025-04-29 09:47:22 +0900

    Paiza 問題集 二重ループメニュー 二次元配列の行ごとの和 C編（paizaランク D 相当）

--- c_rank/double_roop_problems/c/row_sum/row_sum.c
+++ c_rank/double_roop_problems/c/row_sum/row_sum.c
@@ -1,33 +1,24 @@
+// Paiza 問題集 二重ループメニュー 二次元配列の行ごとの和 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/double_roop_problems/double_roop_problems__row_sum
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
     int n, k;
 
     scanf("%d %d", &n, &k);
-    int** ppa = (int**)calloc(n, sizeof(int*));
     for (int i = 0; i < n; i++) {
-        ppa[i] = (int*)calloc(k, sizeof(int*));
-        for (int j = 0; j < k; j++) {
-            scanf("%d", &ppa[i][j]);
-        }
-    }
-
-    for (int i = 0; i < n; i++) {
-        int s = 0;
+        int sum = 0;
 
         for (int j = 0; j < k; j++) {
-            s += ppa[i][j];
+            int a;
+
+            scanf("%d", &a);
+            sum += a;
         }
 
-        printf("%d\n", s);
-   }
-
-    for (int i = 0; i < n; i++) {
-        free(ppa[i]);
+        printf("%d\n", sum);
     }
 
-    free(ppa);
     return 0;
 }
