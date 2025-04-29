24de30db3c1a0582c710a810ea90774821cac88d
kos-y <kosyos.address@gmail.com>
2025-04-29 16:34:15 +0900

    Paiza 問題集 Cランク実力確認問題セット1 電話番号 C編（paizaランク D 相当）

---
+++ c_rank/rank_test_problems_c_0001/c/1/1.c
@@ -0,0 +1,12 @@
+// Paiza 問題集 Cランク実力確認問題セット1 電話番号 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/rank_test_problems_c_0001/rank_test_problems_c_0001__1
+#include <stdio.h>
+
+int main()
+{
+    char a[5], b[5], c[5];
+
+    scanf("%4s %4s %4s", a, b, c);
+    printf("%s-%s-%s\n", a, b, c);
+    return 0;
+}
