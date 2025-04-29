33e9ba305f697ee623d0449c20fdad4ed06ca7fd
kos-y <kosyos.address@gmail.com>
2025-04-29 10:40:32 +0900

    Paiza 問題集 Cランクレベルアップメニュー forループ C編（paizaランク C 相当）

--- c_rank/c_rank_level_up_problems/c/sort/step1.c
+++ c_rank/c_rank_level_up_problems/c/sort/step1.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 Cランクレベルアップメニュー forループ C編（paizaランク C 相当）
+// https://paiza.jp/works/mondai/c_rank_level_up_problems/c_rank_sort_step1
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,10 +9,10 @@ int main()
 {
     int n;
 
-    scanf("%d ", &n);
+    scanf("%d", &n);
     int* a = (int*)calloc(n, sizeof(int));
     for (int i = 0; i < n; i++) {
-        scanf("%d ", &a[i]);
+        scanf("%d", &a[i]);
     }
     
     qsort(a, n, sizeof(int), cmp);
