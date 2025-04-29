3e722e28e9f8d5f938b895d0c8ae59e1968d57fb
kos-y <kosyos.address@gmail.com>
2025-04-29 15:10:41 +0900

    Paiza 問題集 Cランク・スキルチェック過去問題セット 「みかんの仕分け」を解くために：part1 C編（paizaランク D 相当）

--- c_rank/c_rank_skillcheck_archive/c/mikan/01.c
+++ c_rank/c_rank_skillcheck_archive/c/mikan/01.c
@@ -1,17 +1,19 @@
+// Paiza 問題集 Cランク・スキルチェック過去問題セット 「みかんの仕分け」を解くために：part1 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/c_rank_skillcheck_archive/mikan_01
 #include <stdio.h>
 
 int main()
 {
     int n, m;
-    
+
     scanf("%d %d", &n, &m);
     printf("%d %d\n", n, m);
     for (int i = 0; i < m; i++) {
         int w;
-        
+
         scanf("%d", &w);
         printf("%d\n", w);
     }
-    
+
     return 0;
 }
