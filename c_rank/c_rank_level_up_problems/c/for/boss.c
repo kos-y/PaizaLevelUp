1202e2b956c393c820084f430cf8e0fe1e6af44d
kos-y <kosyos.address@gmail.com>
2025-04-29 10:39:25 +0900

    Paiza 問題集 Cランクレベルアップメニュー forループ C編（paizaランク C 相当）

--- c_rank/c_rank_level_up_problems/c/for/boss.c
+++ c_rank/c_rank_level_up_problems/c/for/boss.c
@@ -1,27 +1,26 @@
+// Paiza 問題集 Cランクレベルアップメニュー forループ C編（paizaランク C 相当）
+// https://paiza.jp/works/mondai/c_rank_level_up_problems/c_rank_for_boss
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
     int n, m, k;
 
-    scanf("%d %d %d ", &n, &m, &k);
-    int* c = (int*)calloc(n, sizeof(int));
+    scanf("%d %d %d", &n, &m, &k);
     for (int i = 0; i < n; i++) {
+        int c = 0;
+
         for (int j = 0; j < m; j++) {
             int a;
-            
-            scanf("%d ", &a);
+
+            scanf("%d", &a);
             if (a == k) {
-                c[i]++;
+                c++;
             }
         }
+
+        printf("%d\n", c);
     }
-    
-    for (int i = 0; i < n; i++) {
-        printf("%d\n", c[i]);
-    }
-    
-    free(c);
+
     return 0;
 }
