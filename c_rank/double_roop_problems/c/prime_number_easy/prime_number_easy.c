70a029bb4af46687c05b2ab249cd003936b95f96
kos-y <kosyos.address@gmail.com>
2025-04-29 09:59:56 +0900

    Paiza 問題集 二重ループメニュー 素数の個数 C編（paizaランク C 相当）

--- c_rank/double_roop_problems/c/prime_number_easy/prime_number_easy.c
+++ c_rank/double_roop_problems/c/prime_number_easy/prime_number_easy.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 二重ループメニュー 素数の個数 C編（paizaランク C 相当）
+// https://paiza.jp/works/mondai/double_roop_problems/double_roop_problems__prime_number_easy
 #include <stdio.h>
 #include <stdbool.h>
 
