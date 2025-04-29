4d37defe392d01999ef699a21dc2c47f8518cdfe
kos-y <kosyos.address@gmail.com>
2025-04-29 19:18:51 +0900

    Paiza 問題集 標準出力メニュー 数行の出力 C++編（paizaランク D 相当）

---
+++ c_rank/stdout_primer/cpp/n_line/step2.cpp
@@ -0,0 +1,16 @@
+// Paiza 問題集 標準出力メニュー 数行の出力 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__n_line_step2
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cout << i << endl;
+    }
+
+    return 0;
+}
