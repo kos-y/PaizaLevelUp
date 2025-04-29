aadc513e3086a3c3a5680e36e87bf01a889553d4
kos-y <kosyos.address@gmail.com>
2025-04-29 18:20:12 +0900

    Paiza 問題集 標準出力メニュー 【改行あり出力】1,000 行の出力 C++編（paizaランク D 相当）

---
+++ c_rank/stdout_primer/cpp/newline_digit/boss.cpp
@@ -0,0 +1,13 @@
+// Paiza 問題集 標準出力メニュー 【改行あり出力】1,000 行の出力 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__newline_digit_boss
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 1000; i++) {
+        cout << i << endl;
+    }
+
+    return 0;
+}
