7538cd4ef68e0c1077c6995bb4284b818ae0d76e
kos-y <kosyos.address@gmail.com>
2025-04-29 09:54:38 +0900

    Paiza 問題集 二重ループメニュー 二重ループ：基本編　積の最大 C編（paizaランク D 相当）

--- c_rank/double_roop_problems/c/multi_max/multi_max.c
+++ c_rank/double_roop_problems/c/multi_max/multi_max.c
@@ -1,21 +1,18 @@
+// Paiza 問題集 二重ループメニュー 二重ループ：基本編　積の最大 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/double_roop_problems/double_roop_problems__multi_max
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+
+int* input(int n);
 
 int main()
 {
     int n, k;
 
     scanf("%d %d", &n, &k);
-    int* pa = (int*)calloc(n, sizeof(int));
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &pa[i]);
-    }
-
-    int* pb = (int*)calloc(k, sizeof(int));
-    for (int i = 0; i < k; i++) {
-        scanf("%d", &pb[i]);
-    }
+    int* pa = input(n);
+    int* pb = input(k);
 
     int m = INT_MIN;
     for (int i = 0; i < n; i++) {
@@ -30,4 +27,14 @@ int main()
     free(pa);
     free(pb);
     return 0;
+}
+
+int* input(int n)
+{
+    int* p = (int*)calloc(n, sizeof(int));
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &p[i]);
+    }
+
+    return p;
 }
