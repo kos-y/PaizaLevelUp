c4d2592867737d23760e550d3bbfe42bfd7a5e61
kos-y <kosyos.address@gmail.com>
2025-04-29 15:05:31 +0900

    Paiza 問題集 Cランク・スキルチェック過去問題セット 「野球の審判」を解くために:part5 C編（paizaランク D 相当）

--- c_rank/c_rank_skillcheck_archive/c/umpire/06.c
+++ c_rank/c_rank_skillcheck_archive/c/umpire/06.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 Cランク・スキルチェック過去問題セット 「野球の審判」を解くために:part5 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/c_rank_skillcheck_archive/umpire_06
 #include <stdio.h>
 #include <string.h>
 
@@ -9,12 +11,11 @@ int main()
     for (int i = 0; i < n; i++) {
         char s[7];
 
-        scanf("%s", s);
+        scanf("%6s", s);
         if (strcmp(s, "strike") == 0) {
             printf("%s!\n", s);
             printf("%d\n", ++strike);
-        }
-        else {
+        } else {
             printf("%s\n", s);
         }
     }
