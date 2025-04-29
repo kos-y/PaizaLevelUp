87e57f2031bb3c79f22dc16767d9dac9151fbd25
kos-y <kosyos.address@gmail.com>
2025-04-29 18:58:06 +0900

    Paiza 問題集 標準出力メニュー 【半角スペース区切りの出力】1,000 個の数値を出力 C++編（paizaランク D 相当）

---
+++ c_rank/stdout_primer/cpp/boss.cpp
@@ -0,0 +1,25 @@
+// Paiza 問題集 標準出力メニュー 【半角スペース区切りの出力】1,000 個の数値を出力 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__space_oneline_boss
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> vs(1000);
+    iota(vs.begin(), vs.end(), 1);
+
+    string s
+        = accumulate(
+            vs.begin(), vs.end(),
+            string(),
+            [](const string& a, int b) {
+                return a.empty() ? to_string(b) : a + " " + to_string(b);
+            }
+        );
+
+    cout << s << endl;
+    return 0;
+}
