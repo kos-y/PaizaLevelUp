40b32d43895d2c82e2b678f1ffb4951e513ab6b0
kos-y <kosyos.address@gmail.com>
2025-04-29 14:18:12 +0900

    Paiza 問題集 条件式メニュー 論理演算子の組み合わせ 3 C編（paizaランク C 相当）

--- c_rank/conditions_expression/c/logical_operator/boss.c
+++ c_rank/conditions_expression/c/logical_operator/boss.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 条件式メニュー 論理演算子の組み合わせ 3 C編（paizaランク C 相当）
+// https://paiza.jp/works/mondai/conditions_expression/conditions_expression__logical_operator_boss
 #include <stdio.h>
 
 int main()
