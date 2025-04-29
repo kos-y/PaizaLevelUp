a4664805af4bdd3d476f9a642c19235cd96c6398
kos-y <kosyos.address@gmail.com>
2025-04-29 10:11:31 +0900

    Paiza 問題集 Cランクレベルアップメニュー 入力の配列による保持 C編（paizaランク D 相当）

--- c_rank/c_rank_level_up_problems/c/std_in_out/step4.c
+++ c_rank/c_rank_level_up_problems/c/std_in_out/step4.c
@@ -1,3 +1,5 @@
+// Paiza 問題集 Cランクレベルアップメニュー 入力の配列による保持 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/c_rank_level_up_problems/c_rank_std_in_out_step4
 #include <stdio.h>
 #include <limits.h>
 
