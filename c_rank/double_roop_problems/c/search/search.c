e34ee806a2b6a8863a525662897b4b580e5bf573
kos-y <kosyos.address@gmail.com>
2025-04-29 09:43:02 +0900

    Paiza 問題集 二重ループメニュー 二次元配列での要素の検索 C編（paizaランク D 相当）

--- c_rank/double_roop_problems/c/search/search.c
+++ c_rank/double_roop_problems/c/search/search.c
@@ -1,39 +1,25 @@
+// Paiza 問題集 二重ループメニュー 二次元配列での要素の検索 C編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/double_roop_problems/double_roop_problems__search
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
 
 int main()
 {
     int n, k;
+    bool find = false;
 
     scanf("%d %d", &n, &k);
-    int** ppa = (int**)calloc(n, sizeof(int*));
     for (int i = 0; i < n; i++) {
-        ppa[i] = (int*)calloc(k, sizeof(int*));
         for (int j = 0; j < k; j++) {
-            scanf("%d", &ppa[i][j]);
-        }
-    }
+            int a;
 
-    bool find = false;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < k; j++) {
-            if (ppa[i][j] == 1) {
+            scanf("%d", &a);
+            if (!find && a == 1) {
                 printf("%d %d\n", i + 1, j + 1);
                 find = true;
-                break;
             }
         }
-        
-        if (find) {
-            break;
-        }
-   }
-
-    for (int i = 0; i < n; i++) {
-        free(ppa[i]);
     }
 
-    free(ppa);
     return 0;
 }
