47137447c439fdc273fac4e96b9c14bb1a390ba0
kos-y <kosyos.address@gmail.com>
2025-04-29 20:10:08 +0900

    Paiza 問題集 標準出力メニュー 2 * 2 の出力 C++編（paizaランク D 相当）

---
+++ c_rank/stdout_primer/cpp/2dim_array/step1.cpp
@@ -0,0 +1,11 @@
+// Paiza 問題集 標準出力メニュー 2 * 2 の出力 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__2dim_array_step1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << 0 << " " << 8 << endl;
+    cout << 1 << " " << 3 << endl;
+    return 0;
+}
