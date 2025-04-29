d2231c41aa7fbcb912e8d43acc62ce460ed8b2ed
kos-y <kosyos.address@gmail.com>
2025-04-29 19:01:46 +0900

    Paiza 問題集 標準出力メニュー 2 つの文字列を出力 C++編（paizaランク D 相当）

---
+++ c_rank/stdout_primer/cpp/string_output/step2.cpp
@@ -0,0 +1,10 @@
+// Paiza 問題集 標準出力メニュー 2 つの文字列を出力 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdout_primer/stdout_primer__string_output_step2
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "paiza learning" << endl;
+    return 0;
+}
