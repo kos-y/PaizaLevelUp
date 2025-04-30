7d55134875a3d84c418f89c07a4fddf0de76c59a
kos-y <kosyos.address@gmail.com>
2025-04-30 20:10:47 +0900

    aiza 問題集 標準出力メニュー 標準入力メニュー 3 行の入力 C++編（paizaランク D 相当）

---
+++ c_rank/stdin_primer/cpp/read_line/step3.cpp
@@ -0,0 +1,17 @@
+// Paiza 問題集 標準出力メニュー 標準入力メニュー 3 行の入力 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__read_line_step3
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s, t, u;
+
+    cin >> s;
+    cin >> t;
+    cin >> u;
+    cout << s << endl;
+    cout << t << endl;
+    cout << u << endl;
+    return 0;
+}
