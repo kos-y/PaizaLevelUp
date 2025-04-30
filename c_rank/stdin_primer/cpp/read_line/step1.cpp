182938127d721dbcf6f8cedeac2d0827c14134d8
kos-y <kosyos.address@gmail.com>
2025-04-30 20:05:35 +0900

    Paiza 問題集 標準出力メニュー 標準入力メニュー 1 行の入力 C++編（paizaランク D 相当）

---
+++ c_rank/stdin_primer/cpp/read_line/step1.cpp
@@ -0,0 +1,13 @@
+// Paiza 問題集 標準出力メニュー 標準入力メニュー 1 行の入力 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__read_line_step1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s;
+
+    cin >> s;
+    cout << s << endl;
+    return 0;
+}
