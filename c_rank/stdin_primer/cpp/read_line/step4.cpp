a4b9154638d3190e5d1ab42fbf9e3a277edc35de
kos-y <kosyos.address@gmail.com>
2025-04-30 20:14:24 +0900

    Paiza 問題集 標準入力メニュー 10 行の入力 C++編（paizaランク D 相当）

---
+++ c_rank/stdin_primer/cpp/read_line/step4.cpp
@@ -0,0 +1,16 @@
+// Paiza 問題集 標準入力メニュー 10 行の入力 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__read_line_step4
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 10; i++) {
+        string s;
+
+        cin >> s;
+        cout << s << endl;
+    }
+
+    return 0;
+}
