562e14358f6c4350649eabd4fd3b982412bb6312
kos-y <kosyos.address@gmail.com>
2025-05-04 10:31:09 +0900

    Paiza 問題集 標準入力メニュー 1 行の整数の入力 C++編（paizaランク D 相当）

---
+++ c_rank/stdin_primer/cpp/integer_row/step1.cpp
@@ -0,0 +1,13 @@
+// Paiza 問題集 標準入力メニュー 1 行の整数の入力 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__integer_row_step1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+
+    cin >> a;
+    cout << a << endl;
+    return 0;
+}
