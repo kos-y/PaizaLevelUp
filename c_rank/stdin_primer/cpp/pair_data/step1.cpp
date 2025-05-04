4fd1458f19e73e1a664e2b10f315569814507f0d
kos-y <kosyos.address@gmail.com>
2025-05-04 12:17:26 +0900

    Paiza 問題集 標準入力メニュー 1 つの整数の組の入力 C++編（paizaランク D 相当）

---
+++ c_rank/stdin_primer/cpp/pair_data/step1.cpp
@@ -0,0 +1,13 @@
+// Paiza 問題集 標準入力メニュー 1 つの整数の組の入力 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__pair_data_step1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cin >> a >> b;
+    cout << a << " " << b << endl;
+    return 0;
+}
