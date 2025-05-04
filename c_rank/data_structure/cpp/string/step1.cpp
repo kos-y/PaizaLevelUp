212d6941a1ceed824e34d9117f259de579180094
kos-y <kosyos.address@gmail.com>
2025-05-04 18:01:23 +0900

    Paiza 問題集 データセット選択メニュー とても大きな数値の入力 C++編（paizaランク C 相当）

---
+++ c_rank/data_structure/cpp/string/step1.cpp
@@ -0,0 +1,13 @@
+// Paiza 問題集 データセット選択メニュー とても大きな数値の入力 C++編（paizaランク C 相当）
+// https://paiza.jp/works/mondai/data_structure/data_structure__string_step1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string n;
+
+    cin >> n;
+    cout << n << endl;
+    return 0;
+}
