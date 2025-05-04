e8e2cf9e7af74572ad3ed97bd6bbaf7207a6b896
kos-y <kosyos.address@gmail.com>
2025-05-04 11:22:23 +0900

    Paiza 問題集 標準入力メニュー 2 行目で与えられる N 個の文字列の入力 C++編（paizaランク D 相当）

---
+++ c_rank/stdin_primer/cpp/string_number/step2.cpp
@@ -0,0 +1,21 @@
+// Paiza 問題集 標準入力メニュー 2 行目で与えられる N 個の文字列の入力 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__string_number_step2
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore();
+
+    string s, si;
+    getline(cin, s);
+    istringstream iss(s);
+    while (iss >> si) {
+        cout << si << endl;
+    }
+
+    return 0;
+}
