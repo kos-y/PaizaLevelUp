1c68b08ff674b6a2e257eb808178af3756778fc6
kos-y <kosyos.address@gmail.com>
2025-05-04 10:52:02 +0900

    Paiza 問題集 標準入力メニュー 【整数の半角スペース区切りの入力】1,000個の整数の半角スペース区切りの入力 C++編（paizaランク D 相当）

---
+++ c_rank/stdin_primer/cpp/integer_space/boss.cpp
@@ -0,0 +1,19 @@
+// Paiza 問題集 標準入力メニュー 【整数の半角スペース区切りの入力】1,000個の整数の半角スペース区切りの入力 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__integer_space_boss
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+    string s;
+    getline(cin, s);
+
+    istringstream iss(s);
+    int n;
+    while (iss >> n) {
+        cout << n << endl;
+    }
+
+    return 0;
+}
