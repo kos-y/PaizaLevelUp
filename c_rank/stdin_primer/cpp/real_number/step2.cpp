cc52457985eb7b815f5423f9cfbc346350c7ec25
kos-y <kosyos.address@gmail.com>
2025-05-04 11:47:33 +0900

    Paiza 問題集 標準入力メニュー 2 行目で与えられる N 個の実数の入力 C++編（paizaランク D 相当）

---
+++ c_rank/stdin_primer/cpp/real_number/step2.cpp
@@ -0,0 +1,22 @@
+// Paiza 問題集 標準入力メニュー 2 行目で与えられる N 個の実数の入力 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__real_number_step2
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore();
+
+    string s;
+    getline(cin, s);
+    istringstream iss(s);
+    double a;
+    while (iss >> a) {
+        cout << a << endl;
+    }
+
+    return 0;
+}
