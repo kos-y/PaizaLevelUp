39b34c114f969b2fcde8541b22f2656a25e6b85d
kos-y <kosyos.address@gmail.com>
2025-05-04 21:55:14 +0900

    Paiza 問題集 算術・代入演算メニュー 割り算 C++編（paizaランク D 相当）

---
+++ c_rank/arithmetic_substitution_op/cpp/arithmetic/step3.cpp
@@ -0,0 +1,12 @@
+// Paiza 問題集 算術・代入演算メニュー 割り算 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/arithmetic_substitution_op/arithmetic_substitution_op__arithmetic_step3
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int a = 437326, b = 9085;
+
+    cout << a / b << " " << a % b << endl;
+    return 0;
+}
