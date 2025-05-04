3b21c8f3e472493c53bce3ac37b5246fcac56e12
kos-y <kosyos.address@gmail.com>
2025-05-04 10:40:37 +0900

    Paiza 問題集 標準入力メニュー 100 行の整数の入力 C++編（paizaランク D 相当）

---
+++ c_rank/stdin_primer/cpp/integer_row/step4.cpp
@@ -0,0 +1,17 @@
+// Paiza 問題集 標準入力メニュー 100 行の整数の入力 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__integer_row_step4
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        int a;
+
+        cin >> a;
+        cout << a << endl;
+    }
+
+    return 0;
+}
