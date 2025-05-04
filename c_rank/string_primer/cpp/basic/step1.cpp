a033b10bf885b49012bf45f8c4952b77fa194258
kos-y <kosyos.address@gmail.com>
2025-05-05 08:43:30 +0900

    Paiza 問題集 文字列処理メニュー 文字列の出力 C++編（paizaランク D 相当）

---
+++ c_rank/string_primer/cpp/basic/step1.cpp
@@ -0,0 +1,10 @@
+// Paiza 問題集 文字列処理メニュー 文字列の出力 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/string_primer/basic_step1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "paiza" << endl;
+    return 0;
+}
