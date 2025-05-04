115b8f6652da46eecf16709c713e2cd8e2e3b16a
kos-y <kosyos.address@gmail.com>
2025-05-04 21:50:00 +0900

    Paiza 問題集 算術・代入演算メニュー 足し算 C++編（paizaランク D 相当）

---
+++ c_rank/arithmetic_substitution_op/cpp/arithmetic/step1.cpp
@@ -0,0 +1,10 @@
+// Paiza 問題集 算術・代入演算メニュー 足し算 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/arithmetic_substitution_op/arithmetic_substitution_op__arithmetic_step1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << 1231 + 5178 << endl;
+    return 0;
+}
