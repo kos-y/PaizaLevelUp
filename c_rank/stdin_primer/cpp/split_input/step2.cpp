4bd47f95c433bb01f2b3cb2b7f5fb1ef93b1d435
kos-y <kosyos.address@gmail.com>
2025-05-04 10:19:13 +0900

    Paiza 問題集 標準入力メニュー 半角スペース区切りの 2 つの入力 C++編（paizaランク D 相当）

---
+++ c_rank/stdin_primer/cpp/split_input/step2.cpp
@@ -0,0 +1,14 @@
+// Paiza 問題集 標準入力メニュー 半角スペース区切りの 2 つの入力 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__split_input_step2
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s1, s2;
+
+    cin >> s1 >> s2;
+    cout << s1 << endl;
+    cout << s2 << endl;
+    return 0;
+}
