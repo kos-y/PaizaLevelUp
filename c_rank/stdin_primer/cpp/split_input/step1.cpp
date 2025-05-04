fcea8906bdfd7325ca97f1fe9e810ea24e73714c
kos-y <kosyos.address@gmail.com>
2025-05-04 10:16:03 +0900

    Paiza 問題集 標準入力メニュー 1 つの入力 C++編（paizaランク D 相当）

---
+++ c_rank/stdin_primer/cpp/split_input/step1.cpp
@@ -0,0 +1,13 @@
+// Paiza 問題集 標準入力メニュー 1 つの入力 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__split_input_step1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s;
+
+    cin >> s;
+    cout << s;
+    return 0;
+}
