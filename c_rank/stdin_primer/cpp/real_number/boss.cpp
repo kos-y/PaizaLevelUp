4d9cc66bbe81b3cc73fde86ab1f7cb4bf0b19a74
kos-y <kosyos.address@gmail.com>
2025-05-04 12:15:10 +0900

    Paiza 問題集 標準入力メニュー 1 行目で与えられる N 個の実数の入力 (large) C++編（paizaランク D 相当）

---
+++ c_rank/stdin_primer/cpp/real_number/boss.cpp
@@ -0,0 +1,21 @@
+// Paiza 問題集 標準入力メニュー 1 行目で与えられる N 個の実数の入力 (large) C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__real_number_boss
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    string s;
+    getline(cin, s);
+    istringstream iss(s);
+    double a;
+    while (iss >> a) {
+        cout << a << endl;
+    }
+
+    return 0;
+}
