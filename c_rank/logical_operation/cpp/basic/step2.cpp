7f25166c90824a584c9abd56e5faf52a0fa3dbce
kos-y <kosyos.address@gmail.com>
2025-05-04 22:19:42 +0900

    Paiza 問題集 論理演算メニュー 論理和( OR )の基本 C++編（paizaランク D 相当）

---
+++ c_rank/logical_operation/cpp/basic/step2.cpp
@@ -0,0 +1,13 @@
+// Paiza 問題集 論理演算メニュー 論理和( OR )の基本 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/logical_operation/logical_operation__basic_step2
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cin >> a >> b;
+    cout << (a | b) << endl;
+    return 0;
+}
