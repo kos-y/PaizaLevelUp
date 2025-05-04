b682fb631732e0258f4768b1ffeed4d52619e467
kos-y <kosyos.address@gmail.com>
2025-05-04 21:52:26 +0900

    Paiza 問題集 算術・代入演算メニュー 引き算・掛け算 C++編（paizaランク D 相当）

---
+++ c_rank/arithmetic_substitution_op/cpp/arithmetic/step2.cpp
@@ -0,0 +1,13 @@
+// Paiza 問題集 算術・代入演算メニュー 引き算・掛け算 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/arithmetic_substitution_op/arithmetic_substitution_op__arithmetic_step2
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cin >> a >> b;
+    cout << a - b << " " << a * b << endl;
+    return 0;
+}
