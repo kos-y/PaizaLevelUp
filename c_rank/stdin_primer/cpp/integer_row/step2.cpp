9cab8ac77dedde54d4eee4e62d6379c6e3462654
kos-y <kosyos.address@gmail.com>
2025-05-04 10:36:06 +0900

    Paiza 問題集 標準入力メニュー 2 行の整数の入力 C++編（paizaランク D 相当）

---
+++ c_rank/stdin_primer/cpp/integer_row/step2.cpp
@@ -0,0 +1,17 @@
+// Paiza 問題集 標準入力メニュー 2 行の整数の入力 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/stdin_primer/stdin_primer__integer_row_step2
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 2; i++)
+    {
+        int a;
+
+        cin >> a;
+        cout << a << endl;
+    }
+
+    return 0;
+}
