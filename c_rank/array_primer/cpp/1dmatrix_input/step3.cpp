fed99604911a4506d4c43595a7b117edf7614895
kos-y <kosyos.address@gmail.com>
2025-05-05 21:03:18 +0900

    Paiza 問題集 配列メニュー 配列の入力 3 C++編（paizaランク D 相当）

---
+++ c_rank/array_primer/cpp/1dmatrix_input/step3.cpp
@@ -0,0 +1,19 @@
+// Paiza 問題集 配列メニュー 配列の入力 3 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_primer/array_primer__1dmatrix_input_step3
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int a;
+
+        cin >> a;
+        cout << a << endl;
+    }
+    
+    return 0;
+}
