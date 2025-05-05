40fb40df7f3ee58b2de6cf6d66cb07507259665c
kos-y <kosyos.address@gmail.com>
2025-05-05 19:41:08 +0900

    Paiza 問題集 配列メニュー 配列の入力 1 C++編（paizaランク D 相当）

---
+++ c_rank/array_primer/cpp/1dmatrix_input/step1.cpp
@@ -0,0 +1,15 @@
+// Paiza 問題集 配列メニュー 配列の入力 1 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_primer/array_primer__1dmatrix_input_step1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int v[] = {8, 1, 3, 3, 8, 1, 1, 3, 8, 8};
+
+    for (int n : v) {
+        cout << n << endl;
+    }
+    
+    return 0;
+}
