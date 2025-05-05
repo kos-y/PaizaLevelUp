1772d1f65c69b561c16ed25c52cd1a27128530df
kos-y <kosyos.address@gmail.com>
2025-05-05 09:25:23 +0900

    Paiza 問題集 文字列処理メニュー 文字列の受け取り C++編（paizaランク D 相当）

---
+++ c_rank/string_primer/cpp/basic/step2.cpp
@@ -0,0 +1,13 @@
+// Paiza 問題集 文字列処理メニュー 文字列の受け取り C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/string_primer/basic_step2
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s;
+
+    cin >> s;
+    cout << s << endl;
+    return 0;
+}
