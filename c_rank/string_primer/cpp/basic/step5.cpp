e88d29eba3fec21c94e38723f8f42fd36d16a9da
kos-y <kosyos.address@gmail.com>
2025-05-05 09:31:47 +0900

    Paiza 問題集 文字列処理メニュー 文字列の文字数 C++編（paizaランク D 相当）

---
+++ c_rank/string_primer/cpp/basic/step5.cpp
@@ -0,0 +1,13 @@
+// Paiza 問題集 文字列処理メニュー 文字列の文字数 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/string_primer/basic_step5
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s;
+
+    cin >> s;
+    cout << s.length() << endl;
+    return 0;
+}
