48a6e2e77fb42aeae75af587fd39c27a56063ee9
kos-y <kosyos.address@gmail.com>
2025-05-05 09:59:36 +0900

    Paiza 問題集 文字列処理メニュー 文字列から数値への変換 C++編（paizaランク C 相当）

---
+++ c_rank/string_primer/cpp/normal/step4.cpp
@@ -0,0 +1,13 @@
+// Paiza 問題集 文字列処理メニュー 文字列から数値への変換 C++編（paizaランク C 相当）
+// https://paiza.jp/works/mondai/string_primer/normal_step4
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+    cout << n - 813 << endl;
+    return 0;
+}
