fe1e6d24318e706c7879521995645eaf5347efb7
kos-y <kosyos.address@gmail.com>
2025-05-05 19:35:52 +0900

    Paiza 問題集 配列メニュー 要素数の出力 C++編（paizaランク D 相当）

---
+++ c_rank/array_primer/cpp/1dmatrix_output/step1.cpp
@@ -0,0 +1,12 @@
+// Paiza 問題集 配列メニュー 要素数の出力 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/array_primer/array_primer__1dmatrix_output_step1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int v[] = {5, 1, 3, 4, 5, 12, 6, 8, 1, 3};
+
+    cout << size(v) << endl;
+    return 0;
+}
