2f30bbf40ecc9be5c200be3888732390bd571fc5
kos-y <kosyos.address@gmail.com>
2025-05-05 09:44:12 +0900

    Paiza 問題集 文字列処理メニュー 文字列の連結 C++編（paizaランク D 相当）

---
+++ c_rank/string_primer/cpp/basic/step7.cpp
@@ -0,0 +1,20 @@
+// Paiza 問題集 文字列処理メニュー 文字列の連結 C++編（paizaランク D 相当）
+// https://paiza.jp/works/mondai/string_primer/basic_step7
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        string s;
+
+        cin >> s;
+        cout << s;
+    }
+
+    cout << endl;
+    return 0;
+}
